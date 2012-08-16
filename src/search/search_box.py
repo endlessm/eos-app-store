0b1a5814ff6e3e8f1aafdc041fafc8ea0fdfc1ed
kyle.miller <kyle.miller@asolutions.com>
2012-08-16 14:35:48 -0500

    refactored search box into multiple widgets. kyle

--- src/search/search_box.py
+++ src/search/search_box.py
@@ -29,9 +29,16 @@ class SearchBox(gtk.EventBox):
         self.connect('button-press-event', self.gain_focus)
         self._content = gtk.Fixed()
         
-        self._label = SearchBoxLabel(self.DEFAULT_TEXT, self._emit_browser_signal)
-        self._label.set_size_request(self.WIDTH, self.HEIGHT)
-        self._content.put(self._label, self.LEFT_PADDING, 0)
+        self._label = SearchBoxLabel(self.DEFAULT_TEXT)
+        self._button = SearchBoxButton(self._emit_browser_signal)
+        self._frame = SearchBoxFrame()
+        
+        self._button.connect('button-press-event', self.browser_button_pressed)
+        
+        self._container = SearchBoxContainer(self._button, self._label, self._frame)
+        
+        self._container.set_size_request(self.WIDTH, self.HEIGHT)
+        self._content.put(self._container, self.LEFT_PADDING, 0)
         
         self.add(self._content)
         self._content.show_all()
@@ -41,16 +48,16 @@ class SearchBox(gtk.EventBox):
         self._text_buffer = self._text_view.get_buffer()
         self._text_view.set_wrap_mode(gtk.WRAP_NONE)
         self._text_view.modify_text(gtk.STATE_NORMAL, gtk.gdk.Color('#303030'))
-        self._text_view.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#ffffff')) 
+        self._text_view.modify_base(gtk.STATE_NORMAL, gtk.gdk.Color('#f2f1f1')) 
 
-        self._text_view.set_size_request(self.WIDTH-self.RIGHT_MARGIN - SearchBoxLabel.LEFT_MARGIN, self.HEIGHT-self.BOTTOM_MARGIN)
+        self._text_view.set_size_request(self.WIDTH-self.RIGHT_MARGIN - SearchBoxContainer.LEFT_MARGIN, self.HEIGHT-self.BOTTOM_MARGIN)
         
         self._text_view.hide()
 
         self._text_view.connect("focus-out-event", lambda w, e: self._update_label(w))
         self._text_view.connect("key-press-event", self.handle_keystrokes)
         
-        self._content.put(self._text_view, self._label.LEFT_MARGIN + self.LEFT_PADDING, self._label.TOP_MARGIN)
+        self._content.put(self._text_view, self._label._LEFT_MARGIN + self.LEFT_PADDING, SearchBoxContainer.TOP_MARGIN)
         
     def gain_focus(self, widget, event):
         if not(hasattr(self, "_text_view")):
@@ -78,6 +85,9 @@ class SearchBox(gtk.EventBox):
         self._text_view.hide()
         self._set_label_text(self.DEFAULT_TEXT)
         self._emit_browser_signal(search_text)
+        
+    def browser_button_pressed(self, widget, event):
+        self._emit_browser_signal("")
   
     def _emit_browser_signal(self, search_text):      
         self.emit("launch-search", search_text)
@@ -93,67 +103,82 @@ class SearchBox(gtk.EventBox):
             return True
         return False
 
-class SearchBoxLabel(gtk.Fixed):
+class SearchBoxFrame(gtk.Image):
+    
+    def __init__(self):
+        gtk.Image.__init__(self)
+        
+        self._search_bg_pixbuf = load_pixbuf(image_util.image_path("text_frame_normal.png"))
+        
+        self.set_from_pixbuf(self._search_bg_pixbuf)
+        
+class SearchBoxLabel(gtk.Label):
+
+    def __init__(self, default_text, width=255, left_margin=40, right_padding=16, right_margin=10):
+        gtk.Label.__init__(self)
+        
+        self._LEFT_MARGIN = left_margin
+        self._RIGHT_MARGIN = right_margin
+        self._RIGHT_PADDING = right_padding
+        self._WIDTH=width
+        
+        self.set_text(default_text)
+        self.set_justify(gtk.JUSTIFY_LEFT)
+        self.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color('#303030'))
+        
+    def set_text(self, text):
+        max_width = self._WIDTH - (self._LEFT_MARGIN + self._RIGHT_MARGIN)
+
+        shown_text = ""
+        for letter in text:
+            shown_text += letter
+            layout = self.create_pango_layout(shown_text)
+            text_size = layout.get_pixel_size()[0] 
+            if text_size + self._RIGHT_PADDING >= max_width:
+                shown_text += "..." 
+                break
+
+        super(SearchBoxLabel, self).set_text(shown_text)
+#        self.set_text(shown_text)
+        
+class SearchBoxButton(gtk.EventBox):
+
+    def __init__(self, callback):
+        gtk.EventBox.__init__(self)
+        
+        self._internet_button_callback = callback
+        self.set_visible_window(False)
+        
+        self._internet_pixbuf = load_pixbuf(image_util.image_path("button_browser_normal.png"))
+        self._internet_pixbuf_hover = load_pixbuf(image_util.image_path("button_browser_over.png"))
+        self._internet_pixbuf_down = load_pixbuf(image_util.image_path("button_browser_down.png"))
+
+        self._internet_image = gtk.Image()
+        self.add(self._internet_image)
+        self._internet_image.set_from_pixbuf(self._internet_pixbuf)
+        
+        self.connect("enter-notify-event", lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf_hover))
+        self.connect("leave-notify-event", lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf))
+        self.connect('button-press-event', lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf_down))
+        self.connect('button-release-event',lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf))
+
+    def toggle_image(self, image, pixbuf):
+        image.set_from_pixbuf(pixbuf) 
+        
+class SearchBoxContainer(gtk.Fixed):
     TOP_MARGIN = 5
     LEFT_MARGIN = 40
     RIGHT_PADDING = 16
     RIGHT_MARGIN = 10
     
-    def __init__(self, default_text, callback):
+    def __init__(self, button, label, frame):
         gtk.Fixed.__init__(self)
         
-        self._internet_button_callback = callback
-        
-        self._search_bg_pixbuf = load_pixbuf(image_util.image_path("text_frame_normal.png"))
-        self._internet_pixbuf = load_pixbuf(image_util.image_path("button_browser_normal.png"))
-        self._internet_pixbuf_hover = load_pixbuf(image_util.image_path("button_browser_over.png"))
-        self._internet_pixbuf_down = load_pixbuf(image_util.image_path("button_browser_down.png"))
-        
-        self._image = gtk.Image()
-        self._image.set_from_pixbuf(self._search_bg_pixbuf)
-        self.put(self._image, 0, 0)
+        self.put(frame, 0, 0)
+        self.put(button, 5, 2)
 
-
-        self._internet_event_box = gtk.EventBox()
-        self._internet_event_box.set_visible_window(False)
-        self._internet_image = gtk.Image()
-        self._internet_event_box.add(self._internet_image)
-        self._internet_image.set_from_pixbuf(self._internet_pixbuf)
-        self.put(self._internet_event_box, 5, 2)
-        
-        self._internet_event_box.connect("enter-notify-event", lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf_hover))
-        self._internet_event_box.connect("leave-notify-event", lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf))
-        self._internet_event_box.connect('button-press-event', self.internet_button_pressed)
-        self._internet_event_box.connect('button-release-event',lambda w, e: self.toggle_image(self._internet_image, self._internet_pixbuf))
-    
-        self._label = gtk.Label()
-        self._label.set_text(default_text)
-        self._label.set_justify(gtk.JUSTIFY_LEFT)
-        self._label.modify_fg(gtk.STATE_NORMAL, gtk.gdk.Color('#303030'))
         alignment = gtk.Alignment(0.0, 0.5, 0.0, 0.0)
-        alignment.add(self._label)
+        alignment.add(label)
         
         self.put(alignment, self.LEFT_MARGIN, self.TOP_MARGIN)
-        
-    def internet_button_pressed(self, widget, event):
-        self.toggle_image(self._internet_image, self._internet_pixbuf_down)
-        self._internet_button_callback("")
-        return True
-
-    def toggle_image(self, image, pixbuf):
-        image.set_from_pixbuf(pixbuf) 
-        
-    def set_text(self, text):
-        max_width = self._search_bg_pixbuf.get_width() - (self.LEFT_MARGIN + self.RIGHT_MARGIN)
-
-        shown_text = ""
-        for letter in text:
-            shown_text += letter
-            layout = self._label.create_pango_layout(shown_text)
-            text_size = layout.get_pixel_size()[0] 
-            if text_size + self.RIGHT_PADDING >= max_width:
-                shown_text += "..." 
-                break
-            
-        self._label.set_text(shown_text)
         
