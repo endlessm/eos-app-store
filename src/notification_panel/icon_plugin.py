19a802512ddb38a491285b949c2a085ed6d94a70
srdjan.grubor <srdjan.grubor@asolutions.com>
2012-08-17 14:34:53 -0500

    Made icons have a drop-shadow with cairo -Srdjan

--- src/notification_panel/icon_plugin.py
+++ src/notification_panel/icon_plugin.py
@@ -10,38 +10,41 @@ class IconPlugin(gtk.EventBox):
     def __init__(self, icon_size, icon_name, command):
         super(IconPlugin, self).__init__()
         
+        self.set_visible_window(False)
+        
         self._command = command
         
         self.set_size_request(icon_size, icon_size)
         
-        plugin_image = gtk.Image()
         pixbuf = load_pixbuf(icon_name)
-        scaled_pixbuf = pixbuf.scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
-        
-        shadow_pixbuf = scaled_pixbuf
-
-        overlay = scaled_pixbuf.copy()
-        shadow_pixbuf.composite(
-                                 overlay,
-                                 self.SHADOW_OFFSET, self.SHADOW_OFFSET,
-                                 overlay.get_width()-self.SHADOW_OFFSET, overlay.get_height()-self.SHADOW_OFFSET,
-                                 self.SHADOW_OFFSET, self.SHADOW_OFFSET,
-                                 1, 1,
-                                 gdk.INTERP_BILINEAR,
-                                 255)
-            
-
-
-        
-        plugin_image.set_from_pixbuf(overlay)
+        self._scaled_pixbuf = pixbuf.scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
         
         del pixbuf
-        del scaled_pixbuf
         
-        self.set_visible_window(False)
-        self.add(plugin_image)
+        self.connect("expose-event", self._draw)
+        self.connect("configure-event", self._draw)
         
     def get_launch_command(self):
         return self._command
+    
+    def _draw(self, widget, event):
+        cr = widget.window.cairo_create()
+        
+        # clip to dimensions of widget
+        cr.rectangle(event.area.x, event.area.y,
+                    event.area.width, event.area.height)
+        cr.clip()
+        
+        # Draw shadow
+        cr.set_source_pixbuf(self._scaled_pixbuf, event.area.x + self.SHADOW_OFFSET, event.area.y + self.SHADOW_OFFSET)
+        cr.set_operator(cairo.OPERATOR_DEST_OUT);
+        cr.paint_with_alpha(0.3)
+
+        # Draw icon
+        cr.set_source_pixbuf(self._scaled_pixbuf, event.area.x, event.area.y)
+        cr.set_operator(cairo.OPERATOR_ATOP);
+        cr.paint()
+        
+        return False
     
     
