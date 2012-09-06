81397cff8856c9a0cb2ffcaa103d0a9d15f42316
kyle.miller <kyle.miller@asolutions.com>
2012-09-06 13:18:13 -0500

    refactored threading to a seperate method, enhanced test to mock out threading method so the test won't start a thread and will actually end. Also mocked out get_volume so the test pass on a machine without an audio card, i.e. the build machine. kyle

--- test/notification_panel_test/test_audio_plugin.py
+++ test/notification_panel_test/test_audio_plugin.py
@@ -2,7 +2,20 @@ import unittest
 from mock import Mock
 
 from notification_panel.audio_plugin import AudioSettingsPlugin
+import sys
 
 class AudioSettingsPluginTestCase(unittest.TestCase):
     def test_audio_plugin_launches_settings_when_clicked(self):
-        self.assertEqual('sudo gnome-control-center --class=eos-audio-manager sound', AudioSettingsPlugin(1).get_launch_command())
+        self.assertEqual('sudo gnome-control-center --class=eos-audio-manager sound', MockAudioSettingsPlugin(1).get_launch_command())
+
+class MockAudioSettingsPlugin (AudioSettingsPlugin):
+    
+    def init(self, size):
+        super(AudioSettingsPlugin, self).__init__(size)
+    
+    def _init_thread(self):
+        pass
+    
+    def _get_volume(self):
+        pass
+    
