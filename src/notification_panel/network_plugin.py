eed7e06207ba19e45c0f6800031e62721297d227
matt.irwin <matt.irwin@asolutions.com>
2012-09-12 14:25:37 -0500

    Adding device interaction to test on laptop. Erik and Matt Irwin [endlessm/eos-features-issues#199]

--- src/notification_panel/network_plugin.py
+++ src/notification_panel/network_plugin.py
@@ -1,4 +1,7 @@
+import dbus
+
 from icon_plugin import IconPlugin
+from dbus.mainloop.glib import DBusGMainLoop 
 
 class NetworkSettingsPlugin(IconPlugin):
     COMMAND = 'sudo gnome-control-center --class=eos-network-manager network'
@@ -6,3 +9,7 @@ class NetworkSettingsPlugin(IconPlugin):
     
     def __init__(self, icon_size):
         super(NetworkSettingsPlugin, self).__init__(icon_size, [self.ICON_NAME], self.COMMAND)
+        self.bus = dbus.SystemBus()
+        NetworkManager
+        self.bus.get_object('org.freedesktop/NetworkManager/Devices/eth0')
+        self.bus.add_signal_receiver(self.handle_change, None, self.NM + '.AccessPoint', None, None)
--- test/notification_panel_test/test_network_plugin.py
+++ test/notification_panel_test/test_network_plugin.py
@@ -6,4 +6,6 @@ from notification_panel.network_plugin import NetworkSettingsPlugin
 class NetworkPluginTestCase(unittest.TestCase):
     def test_initially_shortcut_list_is_retrieved_from_app_util_manager(self):
         self.assertEqual('sudo gnome-control-center --class=eos-network-manager network', NetworkSettingsPlugin(1).get_launch_command())
+    
+
 
