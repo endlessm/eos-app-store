fa2d05f336cf604ca54bdff99a4858da5968f267
matt.irwin <matt.irwin@asolutions.com>
2012-09-17 10:11:47 -0500

    mocking out screen_util - Matt I & Jason

--- test/notification_panel_test/test_all_settings_plugin.py
+++ test/notification_panel_test/test_all_settings_plugin.py
@@ -3,19 +3,23 @@ from mock import Mock
 
 from notification_panel.all_settings_plugin import AllSettingsPlugin
 from osapps.app_launcher import AppLauncher
+from util import screen_util
 
 class AllSettingsPluginTestCase(unittest.TestCase):
     def test_settings_does_not_directly_launch_command(self):
+        screen_util.get_width = Mock(return_value=1)
         self.assertIsNone(AllSettingsPlugin(1).get_launch_command())
         
     def test_settings_settings(self):
         AppLauncher.launch = Mock()
+        screen_util.get_width = Mock(return_value=1)
         plugin = AllSettingsPlugin(1)
         plugin._launch_settings(Mock(), Mock())
         AppLauncher.launch.assert_called_once_with('sudo gnome-control-center --class=eos-network-manager')
 
     def test_settings_logout(self):
         AppLauncher.launch = Mock()
+        screen_util.get_width = Mock(return_value=1)
         plugin = AllSettingsPlugin(1)
         plugin._confirm = Mock(return_value = True)
         plugin._logout(Mock(), Mock())
@@ -23,6 +27,7 @@ class AllSettingsPluginTestCase(unittest.TestCase):
 
     def test_settings_restart(self):
         AppLauncher.launch = Mock()
+        screen_util.get_width = Mock(return_value=1)
         plugin = AllSettingsPlugin(1)
         plugin._confirm = Mock(return_value = True)
         plugin._restart(Mock(), Mock())
@@ -30,6 +35,7 @@ class AllSettingsPluginTestCase(unittest.TestCase):
 
     def test_settings_shutdown(self):
         AppLauncher.launch = Mock()
+        screen_util.get_width = Mock(return_value=1)
         plugin = AllSettingsPlugin(1)
         plugin._confirm = Mock(return_value = True)
         plugin._shutdown(Mock(), Mock())
