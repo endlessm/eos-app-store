7e90a2d20addd97a8433fdc31604f2d2a3a6b456
Matt Todd <matt.c.todd@gmail.com>
2012-09-28 09:58:39 -0500

    if no version file available, query the version of endless-os-desktop-widget - Matt T & Srdjan [endlessm/eos-features-issues#369]

--- src/notification_panel/all_settings_model.py
+++ src/notification_panel/all_settings_model.py
@@ -2,9 +2,10 @@ import os
 from osapps.os_util import OsUtil
 
 class AllSettingsModel():
-    VERSION_COMMAND = "dpkg -l endless-os-browser | grep endless | awk \"{print $3}\""
+    VERSION_COMMAND = "dpkg -l endless-os-desktop-widget | grep endless | awk \"{print $3}\""
+    DEFAULT_VERSION_FILE = "/usr/share/endlessm/version.txt"
 
-    def __init__(self, os_util=OsUtil(), version_file="/usr/share/endlessm/version.txt"):
+    def __init__(self, os_util=OsUtil(), version_file=DEFAULT_VERSION_FILE):
         self._version_file = version_file
         self._os_util = os_util
 
