036f63144a75f2658a03d587c04d95a8c0b6cfb8
Matt Todd <matt.c.todd@gmail.com>
2012-09-27 21:42:44 -0500

    added edge cases for when the version file is not available - Matt T [endlessm/eos-features-issues#369]

--- src/notification_panel/all_settings_model.py
+++ src/notification_panel/all_settings_model.py
@@ -1,12 +1,21 @@
+import os
+from osapps.os_util import OsUtil
 
 class AllSettingsModel():
-    def __init__(self, version_file="/usr/share/endlessm/version.txt"):
+    VERSION_COMMAND = "dpkg -l endless-os-browser | grep endless | awk \"{print $3}\""
+
+    def __init__(self, os_util=OsUtil(), version_file="/usr/share/endlessm/version.txt"):
         self._version_file = version_file
+        self._os_util = os_util
 
     def get_current_version(self):
-        version_text = "EndlessOS"
-
-        with open(self._version_file, "r") as f:
-            version_text = f.read()
+        if os.path.exists(self._version_file):
+            with open(self._version_file, "r") as f:
+                version_text = f.read()
+        else:
+            try:
+                version_text = "EndlessOS " + self._os_util.execute(self.VERSION_COMMAND)
+            except:
+                version_text = "EndlessOS"
 
         return version_text
--- test/notification_panel_test/test_all_settings_model.py
+++ test/notification_panel_test/test_all_settings_model.py
@@ -1,5 +1,6 @@
 import unittest
 from subprocess import Popen
+from mock import Mock
 
 from notification_panel.all_settings_model import AllSettingsModel
 
@@ -11,6 +12,35 @@ class TestAllSettingsModel(unittest.TestCase):
         with open(test_file, "w") as f:
             f.write(current_version)
 
-        test_object = AllSettingsModel(test_file)
+        test_object = AllSettingsModel(None, test_file)
+
         self.assertEquals(current_version, test_object.get_current_version())
+
+    def test_get_current_version_uses_output_from_command_line_result(self):
+        current_version = "version from browser"
+
+        mock_os_util = Mock()
+        mock_os_util.execute = Mock(return_value=current_version)
+
+        test_object = AllSettingsModel(mock_os_util, "file_that_doesn't exist.txt")
+
+        self.assertEquals("EndlessOS " + current_version, test_object.get_current_version())
+
+    def test_when_using_command_line_ensure_that_the_correct_command_is_used(self):
+        mock_os_util = Mock()
+        mock_os_util.execute = Mock(return_value="")
+
+        test_object = AllSettingsModel(mock_os_util, "file_that_doesn't exist.txt")
+
+        test_object.get_current_version()
+
+        mock_os_util.execute.assert_called_once_with(AllSettingsModel.VERSION_COMMAND)
+
+    def test_when_using_command_line_and_an_error_occurs_then_just_display_endlessos(self):
+        mock_os_util = Mock()
+        mock_os_util.execute = Mock(side_effect=Exception("boom!"))
+
+        test_object = AllSettingsModel(mock_os_util, "file_that_doesn't exist.txt")
+
+        self.assertEquals("EndlessOS", test_object.get_current_version())
 
