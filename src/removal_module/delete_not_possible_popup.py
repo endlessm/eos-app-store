811b2b7f3a4b8e035f1e2b63ab17727277307fe8
Ljuba Nedeljkovic <ljuba.ned@gmail.com>
2012-10-15 20:54:39 +0200

    Added internationalized string for delete not possible popup. Tweaks to popup appearance. -Ljuba [endlessm/eos-features-issues#303]

--- src/removal_module/delete_not_possible_popup.py
+++ src/removal_module/delete_not_possible_popup.py
@@ -24,18 +24,29 @@ class DeleteNotPossiblePopupWindow():
         self._close.connect("button-release-event", lambda w, e: self.destroy())
         
         self._container = gtk.VBox(False)
-        
+        self._title_label = gtk.Label()
+        self._title_label.set_markup('<span color="#FFFFFF">WARNING!</span>')
+        self._title_label.set_alignment(0.6, 0.5)
+        self._title_label.set_size_request(220, 24)
         self._close_box = gtk.HBox(False)
-        self._close_box.pack_end(self._close, False, False, 0)
+        self._close_box.pack_end(self._close, False, False, 10)
+        self._close_box.pack_start(self._title_label, False, False, 0)
         
+        self._close_box.set_size_request(24, 24)
         self._container.pack_start(self._close_box, True, False, 0)
+        self._bottom_center = gtk.Alignment(0.5, 1.0, 0, 0)
+        self._label_box = gtk.HBox(False)
         
-        self._label_box = gtk.HBox(True)
+        self._label_box.set_size_request(220, 160)
         self._label = gtk.Label()
-        text = 'To delete a folder you have to\n remove all of the items inside\n of it first.\n\n We are just trying to keep\n you safe.'
-        self._label.set_markup('<span color="white">' + text + '</span>')
-        self._label_box.pack_start(self._label, False, False, 0)
-        self._container.pack_start(self._label_box, True, False, 0)
+        self._label.set_line_wrap(True)
+        
+        text = _('To delete a folder you have to\nremove all of the items inside\nof it first.\n\nWe are just trying to\nkeep you safe.')
+        self._label.set_markup('<span color="#FFFFFF">' + text + '</span>')
+        self._label.set_alignment(0.5, 0.5)
+        self._label_box.pack_start(self._label, True, True, 0)
+        self._bottom_center.add(self._label_box)
+        self._container.pack_start(self._bottom_center, True, False, 0)
         
         
         
