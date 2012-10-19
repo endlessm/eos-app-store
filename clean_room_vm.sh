14324fa6a38c3143025fb407d69ca652cf1e3b14
Roddy Shuler <roddy@endlessm.com>
2012-10-19 11:53:23 -0700

    Removed existing /etc/endlessm and /usr/share/endlessm - Roddy and Kyle [endlessm/eos-features-issues#425]

--- clean_room_vm.sh
+++ clean_room_vm.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-#sudo apt-get install python-alsaaudio
+sudo apt-get install python-alsaaudio
 
+sudo rm -rf /etc/endlessm
+sudo rm -rf /usr/share/endlessm
 sudo ln -s $PWD /etc/endlessm
 sudo ln -s $PWD /usr/share/endlessm
 
