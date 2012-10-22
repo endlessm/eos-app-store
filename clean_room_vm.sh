fed602365a0ae8ebbb4b9bf754620e4db751d294
kyle.miller <kyle.miller@asolutions.com>
2012-10-22 14:08:42 -0500

    removed links and changed to env var. kyle [endlessm/eos-features-issues#439]

--- clean_room_vm.sh
+++ clean_room_vm.sh
@@ -2,8 +2,8 @@
 
 sudo apt-get install python-alsaaudio
 
-sudo rm -rf /etc/endlessm
-sudo rm -rf /usr/share/endlessm
-sudo ln -s $PWD /etc/endlessm
-sudo ln -s $PWD /usr/share/endlessm
+#sudo rm -rf /etc/endlessm
+#sudo rm -rf /usr/share/endlessm
+#sudo ln -s $PWD /etc/endlessm
+#sudo ln -s $PWD /usr/share/endlessm
 
--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 export DISPLAY=:0
 
+export ENDLESS_SHARED_DATA_DIR=/home/endlessm/checkout/eos-desktop
+
 nohup python src/endless_os_desktop.py > console.log 2>&1 &
 #echo "kill -9 $!" > kill.sh
 #chmod +x kill.sh
