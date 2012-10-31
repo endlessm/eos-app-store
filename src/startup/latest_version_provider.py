18e2704d79aecae6f93f610c75eb83c0b93d0ab4
Kyle Miller <kyle.miller@asolutions.com>
2012-10-31 13:01:17 -0500

    barebones get_latest_verison impl. kyle and matt t [endlessm/eos-features-issues#377]

--- src/startup/latest_version_provider.py
+++ src/startup/latest_version_provider.py
@@ -1,4 +1,16 @@
+import json
+import urllib2
 
 class LatestVersionProvider():
-    def get_latest_version(self):
+    
+    def __init__(self):
         pass
+    
+    def get_latest_version(self):
+        version_json = self._download_json()
+        version = json.loads(version_json)
+        return version["version"]
+    
+    def _download_json(self):     
+        response = urllib2.urlopen("http://apt.endlessm.com/version.json")
+        return response.read()
--- test/startup_test/latest_version_provider_test.py
+++ test/startup_test/latest_version_provider_test.py
@@ -1,9 +1,13 @@
 import unittest
 from mock import Mock #@UnresolvedImport
-from startup.update_checker import LatestVersionProvider
+from startup.latest_version_provider import LatestVersionProvider
+
 
 class LatestVersionProviderTestCase(unittest.TestCase):
-    def test_(self):
+    def test_get_latest_version(self):
+#        mock_requestor = Mock()
+#        mock_requestor.download_version = Mock(return_value="{\"version\":\"1.0.0\"}")
         test_object = LatestVersionProvider()
-        test_object.get_latest_version()
+        test_object._download_json = Mock(return_value="{\"version\":\"1.0.0\"}")
+        self.assertEquals("1.0.0", test_object.get_latest_version())
         
