286fbc8709c3644b2ad7ff90afe95118128522c2
sgnn7 <sgnn7@sgnn7.org>
2012-11-02 11:49:25 -0500

    Intermediate commit -Srdjan & Roddy

--- src/notification_panel/battery_provider.py
+++ src/notification_panel/battery_provider.py
@@ -7,25 +7,14 @@ import re
 
 #TODO test me
 class BatteryProvider():
-    HAL_DBUS_PATH = 'org.freedesktop.Hal'
-    HAL_DBUS_MANAGER_URI = '/org/freedesktop/Hal/Manager'
-    HAL_DBUS_MANAGER_PATH = HAL_DBUS_PATH + '.Manager'
-    HAL_DBUS_DEVICE_PATH = HAL_DBUS_PATH + '.Device'
-    
-    DBUS_PROPERTY_MODIFIED = 'PropertyModified'
-    
     AC_ADAPTER_PRESENT = 'ac_adapter.present'
     
     BATTERY_DISCHARGING_PROPERTY = 'battery.rechargeable.is_discharging'
     BATTERY_PERCENTAGE_PROPERTY = 'battery.charge_level.percentage'
     BATTERY_REMAINING_TIME_PROPERTY = 'battery.remaining_time'
     
-    def __init__(self, data_bus = dbus, dbus_utilities = DbusUtils()):
-        self._data_bus = data_bus
-
-        self._system_bus = dbus_utilities.get_system_bus()
-        self._hal_manager = self._system_bus.get_object(BatteryProvider.HAL_DBUS_PATH, BatteryProvider.HAL_DBUS_MANAGER_URI)
-        self._hal_manager_interface = data_bus.Interface (self._hal_manager, BatteryProvider.HAL_DBUS_MANAGER_PATH)
+    def __init__(self, dbus_utilities = DbusUtils()):
+        self._dbus_utilities = dbus_utilities
         
         self._battery_interface = None
     
@@ -34,14 +23,9 @@ class BatteryProvider():
         try:
             self._register_battery_callback()
             
-            # Register listener for AC adapter changes
-            ac_adapters = self._hal_manager_interface.FindDeviceByCapability('ac_adapter', dbus_interface=self.HAL_DBUS_MANAGER_PATH)
-            if len(ac_adapters):
-                primary_ac_adapter = ac_adapters[0]
-                self._ac_adapter_interface = self._data_bus.Interface(self._system_bus.get_object(self.HAL_DBUS_PATH, primary_ac_adapter), self.HAL_DBUS_DEVICE_PATH)
-                self._ac_adapter_interface.connect_to_signal(self.DBUS_PROPERTY_MODIFIED, self._battery_modified)
-                
-            self._system_bus.add_signal_receiver(self._handle_hal_event, dbus_interface=self.HAL_DBUS_MANAGER_PATH)
+            self._ac_adapter_interface = self._dbus_utilities.register_property_listener('ac_adapter', self._battery_modified)
+            
+            self._dbus_utilities.add_signal_callback(r'.*battery.*', self._handle_hal_event)
         except Exception as e:
             log.error("Dbus error", e)
             
@@ -50,19 +34,18 @@ class BatteryProvider():
     def get_battery_info(self):
         battery = Battery(None, None, None)
         try:
-            batteries = self._hal_manager_interface.FindDeviceByCapability('battery', dbus_interface=BatteryProvider.HAL_DBUS_MANAGER_PATH)
-            if not len(batteries):
+            if not self._dbus_utilities.has_device_of_type('battery'):
                 return battery
     
             # Full battery throws this exception  
             remaining = None
             try:
-                remaining = self._battery_interface.GetProperty(self.BATTERY_REMAINING_TIME_PROPERTY)
+                remaining = self._dbus_utilities.get_device_property(self._battery_interface, self.BATTERY_REMAINING_TIME_PROPERTY)
             except:
                 pass
             
-            discharging = not self._ac_adapter_interface.GetProperty(self.AC_ADAPTER_PRESENT)
-            level = self._battery_interface.GetProperty(self.BATTERY_PERCENTAGE_PROPERTY)
+            discharging = not self._dbus_utilities.get_device_property(self._ac_adapter_interface, self.AC_ADAPTER_PRESENT)
+            level = self._dbus_utilities.get_device_property(self._battery_interface, self.BATTERY_PERCENTAGE_PROPERTY)
 
             battery = Battery(not discharging, level, remaining)
         except Exception as e:
@@ -75,18 +58,14 @@ class BatteryProvider():
         if self._battery_interface:
             return
         
-        batteries = self._hal_manager_interface.FindDeviceByCapability('battery', dbus_interface=self.HAL_DBUS_MANAGER_PATH)
-        if len(batteries):
-            primary_battery = batteries[0]
-            self._battery_interface = self._data_bus.Interface(self._system_bus.get_object(self.HAL_DBUS_PATH, primary_battery), self.HAL_DBUS_DEVICE_PATH)
-            self._battery_interface.connect_to_signal(self.DBUS_PROPERTY_MODIFIED, self._battery_modified)
-            
+        self._battery_interface = self._dbus_utilities.register_property_listener('battery', self._battery_modified)
+    
+    
+    
     # if battery plugged in, register listener for its status
-    def _handle_hal_event(self, *args, **kwargs):
-        for arg in args:
-            if re.search(r'.*battery.*', arg):
-                self._register_battery_callback()
-                self._battery_modified(0, None)
+    def _handle_hal_event(self):
+            self._register_battery_callback()
+            self._battery_modified(0, None)
         
     def _battery_modified(self, num_changes, property): #@ReservedAssignment
         self._callback_func(self.get_battery_info())
--- src/util/dbus_utils.py
+++ src/util/dbus_utils.py
@@ -1,15 +1,60 @@
 from dbus.glib import DBusGMainLoop
 import dbus
+import re
 
 # TODO test me
 class DbusUtils:
+    HAL_DBUS_PATH = 'org.freedesktop.Hal'
+    HAL_DBUS_MANAGER_URI = '/org/freedesktop/Hal/Manager'
+    HAL_DBUS_MANAGER_PATH = HAL_DBUS_PATH + '.Manager'
+    HAL_DBUS_DEVICE_PATH = HAL_DBUS_PATH + '.Device'
+   
+    DBUS_PROPERTY_MODIFIED = 'PropertyModified'
+        
     _system_bus = None
     
-    @staticmethod
-    def get_system_bus():
+    def __init__(self, data_bus = dbus):
+        self._data_bus = data_bus
+        
         if DbusUtils._system_bus is None:
             loop = DBusGMainLoop(set_as_default=True) 
             dbus.glib.init_threads()
             DbusUtils._system_bus = dbus.SystemBus(mainloop = loop)
-            
+    
+    def get_system_bus(self):
         return DbusUtils._system_bus
+
+    def register_property_listener(self, device_type, callback):
+        hal_manager = self.get_system_bus().get_object(DbusUtils.HAL_DBUS_PATH, DbusUtils.HAL_DBUS_MANAGER_URI)
+        hal_manager_interface = self._data_bus.Interface(hal_manager, 
+                                                         DbusUtils.HAL_DBUS_MANAGER_PATH)
+        
+        devices_of_type = hal_manager_interface.FindDeviceByCapability(device_type, 
+                                                                       dbus_interface=self.HAL_DBUS_MANAGER_PATH)
+        if len(devices_of_type):
+            primary_device = devices_of_type[0]
+            device_interface = self._data_bus.Interface(self.get_system_bus().get_object(self.HAL_DBUS_PATH, 
+                                                                                         primary_device), 
+                                                                                         self.HAL_DBUS_DEVICE_PATH)
+            device_interface.connect_to_signal(self.DBUS_PROPERTY_MODIFIED, 
+                                               callback)
+            
+            return device_interface
+        
+    def add_signal_callback(self, regex_match, callback):
+        def callback_wrapper(self, *args, **kwargs):
+            for arg in args:
+                if re.search(regex_match, arg):
+                    callback()
+
+        self.get_system_bus().add_signal_receiver(callback_wrapper, 
+                                                  dbus_interface=DbusUtils.HAL_DBUS_MANAGER_PATH)
+        
+    def has_device_of_type(self, device_type):
+        self._hal_manager_interface.FindDeviceByCapability(device_type, dbus_interface=DbusUtils.HAL_DBUS_MANAGER_PATH)
+        
+    def get_device_property(self, interface, property_name):
+        try:
+            return interface.GetProperty(property_name)
+        except:
+            return None
