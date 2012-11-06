47641d815864baa5b9dfd76d9f9b62c8fe98b954
Matt Todd <matt.c.todd@gmail.com>
2012-11-05 18:55:14 -0600

    fixed an edge case - Matt T [endlessm/eos-features-issues#377]

--- src/startup/auto_updates/file_synchronizer.py
+++ src/startup/auto_updates/file_synchronizer.py
@@ -5,8 +5,14 @@ class FileSynchronizer():
 
     def files_to_download(self, local_file_list, remote_file_list):
         all_remote = remote_file_list.strip().split("\n")
+
         for local_file in local_file_list.strip().split("\n"):
             if local_file in all_remote:
                 all_remote.remove(local_file)
+
+        for remote in all_remote:
+            if remote == '':
+                all_remote.remove(remote)
+
         return all_remote
         
