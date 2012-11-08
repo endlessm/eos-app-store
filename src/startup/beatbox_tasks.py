1cfca97370beed09125b13183d5c2960168479a3
Erik Wolfe <erik.wolfe@asolutions.com>
2012-11-08 13:09:51 -0600

    Moves beatbox settings to self-explanatory method. Erik [endlessm/eos-features-issues#412]

--- src/startup/beatbox_tasks.py
+++ src/startup/beatbox_tasks.py
@@ -5,9 +5,6 @@ from osapps.os_util import OsUtil
 class BeatboxTasks():
     TARGET_DIR = "Music"
     
-    def _default_music_folder_path(self):
-        return "/usr/share/endlessm/default_music"
-    
     def __init__(self, home_path_provider=HomePathProvider(), default_file_copier=DefaultFileCopier(TARGET_DIR), os_util=OsUtil()):
         self._default_file_copier = default_file_copier
         self._os_util = os_util
@@ -15,4 +12,10 @@ class BeatboxTasks():
 
     def execute(self):
         self._default_file_copier.copy_from(self._default_music_folder_path())
-        self._os_util.execute(["gsettings", "set", "net.launchpad.beatbox.settings", "music-folder", self._home_path])
+        self._initialize_beatbox_settings()
+    
+    def _default_music_folder_path(self):
+        return "/usr/share/endlessm/default_music"
+    
+    def _initialize_beatbox_settings(self):
+        return self._os_util.execute(["gsettings", "set", "net.launchpad.beatbox.settings", "music-folder", self._home_path])
