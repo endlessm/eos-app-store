179539279b3e618dbbb5e256ea2777a74ee6acab
Kyle Miller <kyle.miller@asolutions.com>
2012-11-13 11:03:48 -0600

    moved screen_util to eos_common, removed image_util since it was already moved to eos_common.

--- src/folder/folder_window.py
+++ src/folder/folder_window.py
@@ -1,8 +1,7 @@
 import gtk
 import gettext
 from eos_widgets.image_eventbox import ImageEventBox
-from eos_util import image_util
-from util import screen_util
+from eos_util import image_util, screen_util
 from util.transparent_window import TransparentWindow
 from folder.folder_icons import FolderIcons
 
@@ -10,44 +9,44 @@ gettext.install('endless_desktop', '/usr/share/locale', unicode = True, names=['
 
 class OpenFolderWindow():
     TASKBAR_HEIGHT = 40
-    
+
     def __init__(self, parent, callback, shortcut):
         self._width = screen_util.get_width()
         # TODO fix this
         self._height = 116
-        
+
         self._window = TransparentWindow(parent)
         self._window.set_title(_("Folder"))
-        
+
 #        self._window.set_gravity(gtk.gdk.GRAVITY_SOUTH_WEST)
 #        self._window.move(0, 0)
         self._window.move(0, screen_util.get_height() - self._height - self.TASKBAR_HEIGHT)
 
         self._fancy_container = ImageEventBox((image_util.image_path("open-folder-bg.png"),), self._width)
         self._fancy_container.show()
-        
+
 
         self._fancy_container.set_size_request(self._width,self._height)
         self._window.set_size_request(self._width,self._height)
-        
+
         self._center = gtk.Alignment(.5,0.1,0,0)
         self._center.show()
-        
+
         self._container = gtk.HBox(False)
-        
+
         folder_icons = FolderIcons(shortcut.children())
         self._container.pack_start(folder_icons, False, False, 0)
         folder_icons.connect("application-shortcut-activate", lambda w, app_id, params: callback(app_id, params))
-        
+
         self._center.add(self._container)
         self._fancy_container.add(self._center)
 
         self._window.add(self._fancy_container)
         self._window.show()
-        
+
 
     def show(self):
         self._window.show_all()
-        
+
     def destroy(self):
         self._window.destroy()
--- src/taskbar_panel/taskbar_icon.py
+++ src/taskbar_panel/taskbar_icon.py
@@ -1,32 +1,32 @@
 import gtk
 from gtk import gdk
 
-from util import image_util
+from eos_util import image_util
 
 class TaskbarIcon(gtk.EventBox):
     SELECTED_ICON_PADDING = 3
-    
+
     def __init__(self, task, name, pixbuf, is_selected):
         selected_overlay = image_util.load_pixbuf('taskbar-well.png')
-        self._scaled_selected_overlay = selected_overlay.scale_simple(pixbuf.get_width(), pixbuf.get_height(), gdk.INTERP_BILINEAR) 
-        
+        self._scaled_selected_overlay = selected_overlay.scale_simple(pixbuf.get_width(), pixbuf.get_height(), gdk.INTERP_BILINEAR)
+
         super(TaskbarIcon, self).__init__()
         self.set_visible_window(False)
-        
+
         self._task = task
         self._name = name
         self._is_selected = is_selected
 
         self._icon = gtk.Image()
-        
+
         self.update_task(name, pixbuf, is_selected)
-        
+
         icon_holder = gtk.Alignment(0.5, 0.5, 0, 0)
         icon_holder.set_padding(2, 2, 3, 3)
         icon_holder.add(self._icon)
-        
+
         self.add(icon_holder)
-        
+
     def task(self):
         return self._task
 
@@ -35,12 +35,12 @@ class TaskbarIcon(gtk.EventBox):
 
     def update_task(self, window_name, pixbuf, is_selected):
         self._is_selected = is_selected
-        
+
         self.set_tooltip_text(window_name)
-        
+
         if is_selected:
             overlay = self._scaled_selected_overlay.copy()
-            scaled_pixbuf = pixbuf.scale_simple(pixbuf.get_width()-(self.SELECTED_ICON_PADDING*2), 
+            scaled_pixbuf = pixbuf.scale_simple(pixbuf.get_width()-(self.SELECTED_ICON_PADDING*2),
                                                 pixbuf.get_height()-(self.SELECTED_ICON_PADDING*2),
                                                 gdk.INTERP_BILINEAR)
             scaled_pixbuf.composite(
@@ -51,7 +51,7 @@ class TaskbarIcon(gtk.EventBox):
                                  1, 1,
                                  gdk.INTERP_BILINEAR,
                                  255)
-            
+
             pixbuf = overlay
-            
+
         self._icon.set_from_pixbuf(pixbuf)
