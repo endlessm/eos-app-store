7903df0248cdedae4e29be77f60e720542ecf545
Matt Todd <matt.c.todd@gmail.com>
2012-11-13 15:26:34 -0600

    fixed stupid error - Matt T

--- src/startup/auto_updates/endless_installer.py
+++ src/startup/auto_updates/endless_installer.py
@@ -3,18 +3,20 @@ from eos_log import log
 
 from startup.auto_updates import endpoint_provider
 from startup.auto_updates.force_install_checker import ForceInstallChecker
+from startup.auto_updates.endless_downloader import EndlessDownloader
 
 class EndlessInstaller():
     def __init__(self, os_util=OsUtil(), force_install_checker=ForceInstallChecker()):
         self._os_util = os_util
         self._force_install_checker = force_install_checker
         
-    def install_all_packages(self, package_directory):
+    def install_all_packages(self):
         try:
             log.info("executing pre install script")
             self._os_util.execute(
                                 ["sudo", "/usr/bin/endless_pre_install.sh"])
 
+            package_directory = EndlessDownloader.DEFAULT_DOWNLOAD_DIRECTORY
             log.info("executing install -- dpkg on directory %s" % package_directory)
             output = self._os_util.execute(
                                 ["sudo", "/usr/bin/endless_install_all_packages.sh", package_directory])
