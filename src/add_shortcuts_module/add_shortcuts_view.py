79ed08143fd3a9d1f49b451e5f904ce4260c5ea8
Paul Hawke + Kyle Miller <irwindb+ph+km@asolutions.com>
2012-11-14 14:44:36 -0600

    fixed add shortcut window import that moved to eos-common.

--- src/add_shortcuts_module/add_shortcuts_view.py
+++ src/add_shortcuts_module/add_shortcuts_view.py
@@ -6,22 +6,22 @@ from shortcut.add_remove_shortcut import AddRemoveShortcut
 from util.transparent_window import TransparentWindow
 from osapps.app_shortcut import AppShortcut
 import cairo
-from util import image_util
+from eos_util import image_util
 
 class AddShortcutsView():
     def __init__(self, parent=None, add_remove_widget=None, width=0, height=0):
         self._add_buton_box_width = 120
         self._tree_view_width = 214
-        
+
         self._width = width or parent.allocation.width
         self._height = height or parent.allocation.height
-            
+
         if not add_remove_widget:
             self._add_remove_widget = AddRemoveShortcut(callback=None)
             self._add_remove_widget.show()
         else:
             self._add_remove_widget = add_remove_widget
-        
+
         self._parent = parent
         self._presenter = AddShortcutsPresenter()
         self.window = TransparentWindow(self._parent)
@@ -35,19 +35,19 @@ class AddShortcutsView():
         self.hbox = gtk.HBox()
         self.add_remove_vbox = gtk.VBox()
         self.add_remove_vbox.set_size_request(self._add_buton_box_width, self._height)
-        
+
         self._lc = gtk.Alignment(0.5, 0.5, 0, 0)
         self._add_remove_widget.show()
         self._lc.add(self._add_remove_widget)
-        
+
         self.add_remove_vbox.pack_start(self._lc)
         self.add_remove_vbox.show()
         self.hbox1 = gtk.HBox()
         self.hbox1.set_size_request(self._tree_view_width, self._height)
-        
+
         self.data = self._presenter.get_category_data()
         self.tree = ShortcutCategoryBox(self.data, self.window, self._tree_view_width)
-        
+
         self.hbox1.pack_start(self.tree)
         self.hbox2 = gtk.HBox()
         self.hbox2.set_size_request(self._width - self._tree_view_width - self._add_buton_box_width, self._height)
@@ -60,20 +60,20 @@ class AddShortcutsView():
         self.hbox.pack_end(self.hbox2)
         self.window.add(self.hbox)
         self.show()
-    
+
     def show(self):
         self.window.show_all()
-    
-    
+
+
     def destroy(self, window, event):
         self.window.destroy()
         self._parent.get_presenter().refresh_view()
-        
-    
+
+
     def create_folder(self, folder_name, image_file):
         presenter = self._parent.get_presenter()
         self._presenter.create_directory(folder_name, image_file, presenter)
-    
+
     def _draw_triangle(self, widget, event):
         ctx = self.add_remove_vbox.window.cairo_create()
         image_surface = cairo.ImageSurface.create_from_png(image_util.image_path("inactive_triangle.png"))
