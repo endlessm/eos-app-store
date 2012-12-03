e34a1e5ecdf7ee52456ee0e892bb04b7239a34ca
Kyle Miller <github@kylemiller.com>
2012-12-03 16:34:38 -0600

    fixed broken tests from previous merge

--- test/desktop_test/test_endless_desktop_model.py
+++ test/desktop_test/test_endless_desktop_model.py
@@ -14,12 +14,12 @@ from metrics.time_provider import TimeProvider
 class DesktopModelTestCase(unittest.TestCase):
     def setUp(self):
         self.available_apps = [
-                               App(123, "app 1", "", "", "", False, False, True), 
-                               App(234, "app 2", "", "", "", False, False, True), 
-                               App(345, "app 3", "", "", "", False, False, True), 
-                               App(456, "app 4", "", "", "", False, False, False), 
-                               App(567, "app 5", "", "", "", False, False, True), 
-                               App(890, "app 6", "", "", "", False, False, True), 
+                               App(123, "app 1", "", "", "", False, False, True),
+                               App(234, "app 2", "", "", "", False, False, True),
+                               App(345, "app 3", "", "", "", False, False, True),
+                               App(456, "app 4", "", "", "", False, False, False),
+                               App(567, "app 5", "", "", "", False, False, True),
+                               App(890, "app 6", "", "", "", False, False, True),
                                ]
         self.available_app_shortcuts = [
                                         AppShortcut(123, "App 1", "", []),
@@ -37,109 +37,109 @@ class DesktopModelTestCase(unittest.TestCase):
         self.mock_time_provider = Mock(TimeProvider)
         self.mock_desktop_preferences = Mock(DesktopPreferencesDatastore)
         self.testObject = EndlessDesktopModel(self.mock_desktop_locale_datastore,
-                                              self.mock_desktop_preferences, 
-                                              self.mock_app_datastore, 
+                                              self.mock_desktop_preferences,
+                                              self.mock_app_datastore,
                                               self.mock_app_launcher,
                                               self.mock_feedback_manager,
-                                              self.mock_time_provider, 
+                                              self.mock_time_provider,
                                               )
-    
+
     def test_initially_shortcut_list_is_retrieved_from_app_util_manager(self):
         self.assertEqual(self.available_apps, self.testObject.get_shortcuts())
 
     def test_execute_app_with_id_calls_launc_app_on_app_util(self):
         params = []
         self.testObject.execute_app('123', params)
-        
+
         self.mock_app_launcher.launch.assert_called_once_with("eog", params)
-        
+
     def test_execute_app_with_cannot_find_app_no_exception(self):
         self.mock_app_datastore = Mock(AppDatastore)
         self.mock_app_datastore.get_app_by_key = Mock(return_value=None)
-        self.testObject = EndlessDesktopModel(self.mock_desktop_locale_datastore, 
-                                              self.mock_desktop_preferences, 
-                                              self.mock_app_datastore, 
+        self.testObject = EndlessDesktopModel(self.mock_desktop_locale_datastore,
+                                              self.mock_desktop_preferences,
+                                              self.mock_app_datastore,
                                               self.mock_app_launcher,
                                               self.mock_feedback_manager,
                                               self.mock_time_provider,
                                               )
-        
+
         params = []
         self.testObject.execute_app('123', params)
-        
+
         self.assertFalse(self.mock_app_launcher.launch.called)
-        
+
     def test_launch_search_launches_browser_with_search_string(self):
         search_string = "foo"
-        
+
         self.testObject.launch_search(search_string)
-        
+
         self.mock_app_launcher.launch_browser.assert_called_once_with(search_string)
 
     def test_get_background_delegates_to_preferences_datastore(self):
-        self.testObject.get_background_pixbuf()
-        
-        self.mock_desktop_preferences.get_background_pixbuf.assert_called()
+        self.testObject.get_background_image()
+
+        self.mock_desktop_preferences.get_background_image.assert_called()
 
     def test_set_background_delegates_to_preferences_datastore(self):
         expected = "/foo/whatever/blah"
         self.testObject.set_background(expected)
-        
+
         self.mock_desktop_preferences.set_background.assert_called_once_with(expected)
-        
+
     def test_get_default_background_delegates_to_preferences_datastore(self):
         self.testObject.get_default_background()
-        
+
         self.mock_desktop_preferences.get_default_background.assert_called()
-        
+
     def test_delete_shortcut_exists(self):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=self.available_app_shortcuts)
         app1 = self.available_app_shortcuts[0]
         self.assertEqual(self.testObject.delete_shortcut(app1), True, 'Delete shortcut which exists FAILED.')
-    
+
     def test_delete_shortcut_does_not_exist(self):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=self.available_app_shortcuts)
         invalid_app = AppShortcut(223, "App 5", "", [])
         self.assertEqual(self.testObject.delete_shortcut(invalid_app), False, 'Delete shortcut which does not exist FAILED.')
-    
+
     def test_delete_shortcut_exception_handled(self):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=self.available_app_shortcuts)
         self.mock_desktop_locale_datastore.set_all_shortcuts = Mock(side_effect=Exception('Booom!'))
         #self.mock_desktop_locale_datastore.set_all_shortcuts()
         app1 = self.available_app_shortcuts[0]
         self.assertEqual(self.testObject.delete_shortcut(app1), False, 'Delete shortcut, exception handled FAILED.')
-        
+
     def test_relocate_shortcut_invalid_source(self):
         ret = self.testObject.relocate_shortcut(None, 'irrelevant')
         self.assertEqual(ret, False)
-    
+
     def test_relocate_shortcut_invalid_source(self):
         ret = self.testObject.relocate_shortcut(None, 'irrelevant')
-        self.assertEqual(ret, False)    
-        
+        self.assertEqual(ret, False)
+
     def test_relocate_shortcut_move_to_root(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
         app1.add_child(app2)
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1])
         self.testObject._relocate_shortcut_to_root = Mock(return_value=[app1])
-        
+
         ret = self.testObject.relocate_shortcut(app2, None)
-        
+
         self.testObject._relocate_shortcut_to_root.assert_called_once_with(app2)
-        
+
     def test_relocate_shortcut_move_to_root_source_already_on_root(self):
         app1 = AppShortcut('', 'app1', '')
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1])
         self.testObject._relocate_shortcut_to_root = Mock()
         self.testObject._relocate_shortcut_to_folder = Mock()
-        
+
         ret = self.testObject.relocate_shortcut(app1, None)
-        
+
         self.assertFalse(self.testObject._relocate_shortcut_to_root.called)
         self.assertFalse(self.testObject._relocate_shortcut_to_folder.called)
         self.assertEqual(ret, False)
-        
+
     def test_relocate_shortcut_move_to_folder(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
@@ -147,9 +147,9 @@ class DesktopModelTestCase(unittest.TestCase):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1])
         self.testObject._relocate_shortcut_to_root = Mock()
         self.testObject._relocate_shortcut_to_folder = Mock()
-        
+
         ret = self.testObject.relocate_shortcut(app1, app2)
-        
+
         self.testObject._relocate_shortcut_to_folder.assert_called_once_with(app1, app2)
         self.assertFalse(self.testObject._relocate_shortcut_to_root.called)
 
@@ -160,34 +160,34 @@ class DesktopModelTestCase(unittest.TestCase):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1])
         self.mock_desktop_locale_datastore.set_all_shortcuts = Mock()
         app3.add_child(app2)
-        
+
         ret = self.testObject._relocate_shortcut_to_root(app2)
-        
+
         self.mock_desktop_locale_datastore.set_all_shortcuts.assert_called_once_with([app1, app2])
         self.assertTrue(ret)
         self.assertEqual(app3.children(), [])
-        
+
     def test_relocate_shortcut_to_folder_not_desk_no_parent(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[])
         self.mock_desktop_locale_datastore.set_all_shortcuts = Mock()
-        
+
         ret = self.testObject._relocate_shortcut_to_folder(app1, app2)
         self.assertFalse(ret)
         self.assertFalse(self.mock_desktop_locale_datastore.set_all_shortcuts.called)
-        
+
     def test_relocate_shortcut_to_folder_on_desk_no_parent(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1, app2])
         self.mock_desktop_locale_datastore.set_all_shortcuts = Mock()
-        
+
         ret = self.testObject._relocate_shortcut_to_folder(app1, app2)
         self.assertTrue(ret)
         self.mock_desktop_locale_datastore.set_all_shortcuts.assert_called_once_with([app2])
         self.assertEqual(app2.children(), [app1])
-        
+
     def test_relocate_shortcut_to_folder_have_parent_to_folder(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
@@ -195,25 +195,25 @@ class DesktopModelTestCase(unittest.TestCase):
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1])
         self.mock_desktop_locale_datastore.set_all_shortcuts = Mock()
         app3.add_child(app1)
-        
+
         all_shortcuts = self.mock_desktop_locale_datastore.get_all_shortcuts()
-        
+
         ret = self.testObject._relocate_shortcut_to_folder(app1, app2)
         self.assertTrue(ret)
         self.assertEqual(app3.children(), [])
         self.assertEqual(app2.children(), [app1])
         self.mock_desktop_locale_datastore.set_all_shortcuts.assert_called_once_with(all_shortcuts)
-        
+
     def test_relocate_shortcut_to_folder_destination(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
         self.mock_desktop_locale_datastore.get_all_shortcuts = Mock(return_value=[app1, app2])
-        
+
         ret = self.testObject.relocate_shortcut(app1, app2)
         self.assertEqual(ret, True)
         self.assertEqual(app1.parent(), app2)
         self.assertTrue(app1 in app2.children())
-        
+
     def test_relocate_shortcut_to_desk_destination(self):
         app1 = AppShortcut('', 'app1', '')
         app2 = AppShortcut('', 'app2', '')
@@ -221,7 +221,7 @@ class DesktopModelTestCase(unittest.TestCase):
             return_value=[app1, app2]
             )
         app1._parent = app2
-        
+
         ret = self.testObject.relocate_shortcut(app1, None)
         self.assertEqual(ret, True)
         self.assertEqual(app1.parent(), None)
--- test/desktop_test/test_endless_desktop_presenter.py
+++ test/desktop_test/test_endless_desktop_presenter.py
@@ -4,31 +4,31 @@ from desktop.endless_desktop_presenter import DesktopPresenter
 from osapps.app_shortcut import AppShortcut
 
 class TestEndlessDesktopPresenter(unittest.TestCase):
-    
+
     def setUp(self):
         self.mock_model = Mock()
         self.mock_view = Mock()
-        
+
         self.testObject = DesktopPresenter(self.mock_view, self.mock_model)
-    
+
     def tearDown(self):
         self.testObject._view.destroy()
-    
-    
+
+
     @patch.object(DesktopPresenter, 'refresh_view')
     def test_initially_presenter_forces_a_refresh(self, refresh_view):
         DesktopPresenter(Mock(), Mock())
-        
+
         refresh_view.assert_called_once_with()
-    
+
     def test_activate_item_invokes_process(self):
         shortcut = Mock()
         params = []
-        
+
         self.testObject.activate_item(shortcut, params)
-        
+
         self.mock_model.execute_app.assert_called_once_with(shortcut, params)
-        
+
     def test_refresh_view_updates_view(self):
         mock_shortcuts = [Mock(), Mock()]
         mock_menus = [Mock(), Mock()]
@@ -36,62 +36,62 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         self.mock_view.populate_popups = Mock()
         self.mock_model.get_shortcuts = Mock(return_value=mock_shortcuts)
         self.mock_model.get_menus = Mock(return_value=mock_menus)
-        
+
         self.testObject.refresh_view()
-        
+
         self.mock_view.refresh.assert_called_once_with(mock_shortcuts)
-        
+
     def test_submit_feedback_updates_model(self):
         message = "some text"
         is_bug = True
-        
+
         self.testObject.submit_feedback(message, is_bug)
-        
+
         self.mock_model.submit_feedback.assert_called_once_with(message, is_bug)
-    
+
     def test_if_refresh_is_occuring_already_do_not_redraw_again(self):
         self.mock_view.refresh = None
         self.testObject._is_refreshing = True
-        
+
         try:
             self.testObject.refresh_view()
             """Pass"""
         except:
             self.fail("Should not have thrown any errors as no code should have executed")
-            
+
     def test_launch_search_launches_search(self):
         search_string = "blah"
-        
+
         self.testObject.launch_search(search_string)
-        
+
         self.mock_model.launch_search.assert_called_once_with(search_string)
-        
+
     def test_change_background_sets_background(self):
         self.mock_model.reset_mock()
         self.mock_view.reset_mock()
-        
+
         pixbuf = "pixbuf"
-        self.mock_model.get_background_pixbuf = Mock(return_value=pixbuf)
-        
+        self.mock_model.get_background_image = Mock(return_value=pixbuf)
+
         self.testObject.change_background(pixbuf)
-        
+
         self.mock_model.set_background.assert_called_once_with(pixbuf)
-        self.mock_view.set_background_pixbuf.assert_called_once_with(pixbuf)
-        
+        self.mock_view.set_background_image.assert_called_once_with(pixbuf)
+
     def test_revert_background_sets_background_to_default(self):
         filename = "default"
         self.mock_model.get_default_background = Mock(return_value=filename)
         self.testObject.change_background = Mock()
 
         self.testObject.revert_background()
-        
+
         self.testObject.change_background.assert_called_once_with(filename)
-    
+
     def test_delete_shortcut(self):
         shortcut = "shortcut"
         self.testObject.delete_shortcut(shortcut)
         self.mock_model.delete_shortcut.assert_called_once_with(shortcut)
-    
+
     def test_rename_shortcut(self):
         shortcut = AppShortcut(123, "App 1", "", [])
         new_name = 'Blah'
@@ -100,27 +100,27 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         self.mock_model.get_shortcuts.assert_called_once()
         self.mock_model.set_shortcuts.assert_called_once()
         self.assertEqual(changed_shortcut.name(), new_name)
-    
+
     def test_check_shortcut_name(self):
         shortcut1 = AppShortcut(123, "App", "", [])
         shortcut2 = AppShortcut(123, "App 1", "", [])
         self.mock_model.get_shortcuts = Mock(return_value=[shortcut1, shortcut2])
         expected_value = 'App 2'
         self.assertEqual(expected_value, self.testObject.check_shortcut_name('App', self.mock_model.get_shortcuts()))
-        
+
     def test_relocate_item(self):
         self.mock_model.relocate_shortcut = Mock(return_value=True)
         source_shortcut = Mock()
         folder_shortcut = Mock()
-        
+
         ret = self.testObject.relocate_item(source_shortcut, folder_shortcut)
         self.assertEqual(ret, True)
-        
+
         self.mock_model.relocate_shortcut.assert_called_once_with(
-            source_shortcut, 
+            source_shortcut,
             folder_shortcut
             )
-            
+
     def test_get_shortcut_by_name(self):
         app1 = Mock()
         app1.name = Mock(return_value='app 1')
@@ -129,40 +129,40 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         app3 = Mock()
         app3.name = Mock(return_value='app 3')
         self.testObject._model.get_shortcuts = Mock(return_value=[app1, app2, app3])
-    
+
         app_ret = self.testObject.get_shortcut_by_name('app 1')
         self.assertEqual(app_ret, app1)
-        
+
         app_ret = self.testObject.get_shortcut_by_name('app 2')
         self.assertEqual(app_ret, app2)
-        
+
         app_ret = self.testObject.get_shortcut_by_name('app 3')
         self.assertEqual(app_ret, app3)
-        
+
         app_ret = self.testObject.get_shortcut_by_name('app 4')
         self.assertEqual(app_ret, None)
-        
-        
+
+
     def test_rearrange_shortcuts_no_relocate(self):
         self.testObject._model.get_shortcuts = Mock(return_value=None)
         source_shortcut = Mock()
         source_shortcut.parent = Mock(return_value = None)
         self.testObject.relocate_item = Mock()
-    
+
         self.testObject.rearrange_shortcuts(source_shortcut, None, None)
-        
+
         self.assertFalse(self.testObject.relocate_item.called)
-    
+
     def test_rearrange_shortcuts_relocate(self):
         self.testObject._model.get_shortcuts = Mock(return_value=None)
         source_shortcut = Mock()
         source_shortcut.parent = Mock(return_value = Mock())
         self.testObject.relocate_item = Mock()
-    
+
         self.testObject.rearrange_shortcuts(source_shortcut, None, None)
-        
+
         self.testObject.relocate_item.assert_called_once_with(source_shortcut, None)
-        
+
     def test_rearrange_shortcuts_left_destination(self):
         all_shortcuts = Mock()
         self.testObject._model.get_shortcuts = Mock(return_value=all_shortcuts)
@@ -172,9 +172,9 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         self.testObject.move_item_left = Mock()
         self.testObject.move_item = Mock()
         self.testObject.move_item_right = Mock()
-        
+
         self.testObject.rearrange_shortcuts(source_shortcut, left_shortcut, None)
-        
+
         self.testObject.move_item_left.assert_called_once_with(source_shortcut, left_shortcut, all_shortcuts)
         self.testObject.move_item.assert_called_once_with(all_shortcuts)
         self.assertFalse(self.testObject.move_item_right.called)
@@ -188,9 +188,9 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         self.testObject.move_item_right = Mock()
         self.testObject.move_item = Mock()
         self.testObject.move_item_left = Mock()
-        
+
         self.testObject.rearrange_shortcuts(source_shortcut, None, right_shortcut)
-        
+
         self.testObject.move_item_right.assert_called_once_with(source_shortcut, right_shortcut, all_shortcuts)
         self.testObject.move_item.assert_called_once_with(all_shortcuts)
         self.assertFalse(self.testObject.move_item_left.called)
@@ -200,60 +200,60 @@ class TestEndlessDesktopPresenter(unittest.TestCase):
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_1, sc_2, sc_3]
-    
+
         self.testObject.move_item_right(sc_1, sc_3, all_shortcuts)
-        
+
         self.assertEqual(all_shortcuts, [sc_2, sc_1, sc_3])
-        
+
     def test_move_item_right_source_not_on_desk(self):
         sc_1 = Mock()
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_2, sc_3]
-    
+
         self.testObject.move_item_right(sc_1, sc_3, all_shortcuts)
-        
+
         self.assertEqual(all_shortcuts, [sc_2, sc_1, sc_3])
-        
+
     def test_move_item_left_source_on_desk_place_on_end(self):
         sc_1 = Mock()
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_1, sc_2, sc_3]
-    
+
         self.testObject.move_item_left(sc_1, sc_3, all_shortcuts)
-        
+
         self.assertEqual(all_shortcuts, [sc_2, sc_3, sc_1])
-        
+
     def test_move_item_left_source_on_desk_place_in_between(self):
         sc_1 = Mock()
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_1, sc_2, sc_3]
-    
+
         self.testObject.move_item_left(sc_1, sc_2, all_shortcuts)
-        
+
         self.assertEqual(all_shortcuts, [sc_2, sc_1, sc_3])
-        
+
     def test_move_item_left_source_not_on_desk_place_on_end(self):
         sc_1 = Mock()
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_2, sc_3]
-    
+
         self.testObject.move_item_left(sc_1, sc_3, all_shortcuts)
-        
+
         self.assertEqual(all_shortcuts, [sc_2, sc_3, sc_1])
-        
+
     def test_move_item_left_source_not_on_desk_place_in_between(self):
         sc_1 = Mock()
         sc_2 = Mock()
         sc_3 = Mock()
         all_shortcuts = [sc_1, sc_2, sc_3]
-    
-        self.testObject.move_item_left(sc_1, sc_2, all_shortcuts)
-        
-        self.assertEqual(all_shortcuts, [sc_2, sc_1, sc_3])
-        
 
-        
+        self.testObject.move_item_left(sc_1, sc_2, all_shortcuts)
+
+        self.assertEqual(all_shortcuts, [sc_2, sc_1, sc_3])
+
+
+
