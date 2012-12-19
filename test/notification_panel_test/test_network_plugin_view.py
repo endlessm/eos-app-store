a4356e673c66856cf4f62c95870bd77d132809d9
matt.irwin <matt.irwin@asolutions.com>
2012-12-19 12:47:03 -0600

    Remove view test. [endlessm/eos-features-issues#491]

--- test/notification_panel_test/test_network_plugin_view.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from mock import Mock
-from notification_panel.network_plugin_view import NetworkPluginView
-
-class NetworkPluginViewTestCase(unittest.TestCase):
-    def test_clicking_network_icon_opens_network_settings(self):
-        self.assertEqual('sudo gnome-control-center network', NetworkPluginView(Mock(), 1).get_launch_command())
