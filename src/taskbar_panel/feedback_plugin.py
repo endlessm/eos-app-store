fdeb874a25eca20412e9402d70bc084b80ea6a4c
Roddy Shuler <roddy@endlessm.com>
2012-12-21 15:58:28 -0800

    Added hover and down states to feedback icon - Roddy [endlessm/eos-features-issues#331]

--- src/taskbar_panel/feedback_plugin.py
+++ src/taskbar_panel/feedback_plugin.py
@@ -6,13 +6,20 @@ from eos_util.image_util import load_pixbuf
 class FeedbackPlugin(gtk.EventBox):
     def __init__(self, icon_size):
         super(FeedbackPlugin, self).__init__()
-        feedback_icon = gtk.Image()
-        pixbuf = load_pixbuf('report-icon_normal.png')
-        scaled_pixbuf = pixbuf.scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
-        feedback_icon.set_from_pixbuf(scaled_pixbuf)
-
-        del pixbuf
-        del scaled_pixbuf
-
+        
+        self._pixbuf_normal = load_pixbuf('report-icon_normal.png').scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
+        self._pixbuf_hover = load_pixbuf('report-icon_hover.png').scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
+        self._pixbuf_down = load_pixbuf('report-icon_down.png').scale_simple(icon_size, icon_size, gdk.INTERP_BILINEAR)
+        
+        self._feedback_icon = gtk.Image()
+        self._feedback_icon.set_from_pixbuf(self._pixbuf_normal)
         self.set_visible_window(False)
-        self.add(feedback_icon)
+        self.add(self._feedback_icon)
+
+        self.connect("enter-notify-event", lambda w, e: self.toggle_image(self._feedback_icon, self._pixbuf_hover))
+        self.connect("leave-notify-event", lambda w, e: self.toggle_image(self._feedback_icon, self._pixbuf_normal))
+        self.connect('button-press-event', lambda w, e: self.toggle_image(self._feedback_icon, self._pixbuf_down))
+        self.connect('button-release-event',lambda w, e: self.toggle_image(self._feedback_icon, self._pixbuf_norma))
+
+    def toggle_image(self, image, pixbuf):
+        image.set_from_pixbuf(pixbuf)
