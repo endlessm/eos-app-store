aab08fe959e50cf534c79748127f7f39e20b9649
Srdjan Grubor <sgnn7@sgnn7.org>
2012-12-31 16:52:57 -0600

    Fixed some breaking tests -Srdjan & Myles

--- src/notification_panel/network_plugin.py
+++ src/notification_panel/network_plugin.py
@@ -5,8 +5,11 @@ from network_plugin_presenter import NetworkPluginPresenter
 
 class NetworkSettingsPlugin(NotificationPlugin):
     COMMAND = 'sudo gnome-control-center network'
-    
+
     def __init__(self, icon_size):
         super(NetworkSettingsPlugin, self).__init__(self.COMMAND)
+        self._create_mvp()
+
+    def _create_mvp(self):
         self._presenter = NetworkPluginPresenter(NetworkPluginView(self, icon_size), NetworkManager.from_dbus())
 
--- test/notification_panel_test/test_network_plugin.py
+++ test/notification_panel_test/test_network_plugin.py
@@ -3,6 +3,8 @@ from notification_panel.network_plugin import NetworkSettingsPlugin
 
 class NetworkPluginTestCase(unittest.TestCase):
     def test_execute_calls_display_menu(self):
+        NetworkSettingsPlugin._create_mvp = Mock(return_value=None)
         test_object = NetworkSettingsPlugin(22)
         self.assertEqual('sudo gnome-control-center network', test_object.get_launch_command())
+        self.assertTrue(test_object._create_mvp.called)
 
