784f9d33f9a630cda4c9398f3845c737c2f80f7a
Matt Irwin <irwinmb14@yahoo.com>
2013-01-04 17:13:20 -0600

    delete and restore installed applications json file during initial start after install - Matt I & Kyle

--- src/startup/delete_desktop_state_task.py
+++ src/startup/delete_desktop_state_task.py
@@ -1,8 +1,12 @@
 import os
+import shutil
 
 class DeleteDesktopStateTask:
-    def __init__(self, os_util=os):
+    def __init__(self, os_util=os, sh_util=shutil):
         self._os_util = os_util
+        self._sh_util = sh_util
         
     def execute(self):
         self._os_util.remove(os.path.expanduser("~/.endlessm/desktop.json"))
+        self._os_util.remove(os.path.expanduser("~/.endlessm/installed_applications.json"))
+        self._sh_util.copyfile("/etc/endlessm/installed-applications.json", "~/.endlessm/installed_applications.json")
--- test/startup_test/delete_desktop_state_task_test.py
+++ test/startup_test/delete_desktop_state_task_test.py
@@ -8,9 +8,15 @@ import os
 class DeleteDesktopStateTaskTest(unittest.TestCase):
     def setUp(self):
         self._mock_os_utils = Mock()
-        
-        self._test_object = DeleteDesktopStateTask(self._mock_os_utils)
-    
+        self._mock_sh_utils = Mock()
+
+        self._test_object = DeleteDesktopStateTask(self._mock_os_utils, self._mock_sh_utils)
+
     def test_delete_state_removes_state_file(self):
         self._test_object.execute()
-        self._mock_os_utils.remove.assert_called_once_with(os.path.expanduser("~/.endlessm/desktop.json"))
+        
+        calls = [call(os.path.expanduser("~/.endlessm/desktop.json")), call(os.path.expanduser("~/.endlessm/installed_applications.json"))]
+        self._mock_os_utils.remove.assert_has_calls(calls)
+        self._mock_sh_utils.copyfile.assert_called_once_with("/etc/endlessm/installed-applications.json", "~/.endlessm/installed_applications.json")
+        
+        
