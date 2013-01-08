235a31a4260dda320e8e8866205efcc541403d55
Kyle Miller <kyle.miller@asynchrony.com>
2013-01-07 19:41:38 -0600

    fixed issue with no expand user. kyle

--- src/startup/delete_desktop_state_task.py
+++ src/startup/delete_desktop_state_task.py
@@ -1,16 +1,16 @@
 import os
 import shutil
-from eos_log import log 
+from eos_log import log
 
 class DeleteDesktopStateTask:
     def __init__(self, os_util=os, sh_util=shutil):
         self._os_util = os_util
         self._sh_util = sh_util
-        
+
     def execute(self):
-        
-        self._sh_util.copyfile("/etc/endlessm/installed_applications.json", "~/.endlessm/installed_applications.json")
-        
+
+        self._sh_util.copyfile("/etc/endlessm/installed_applications.json", os.path.expanduser("~/.endlessm/installed_applications.json"))
+
         try:
             self._os_util.remove(os.path.expanduser("~/.endlessm/desktop.json"))
         except:
