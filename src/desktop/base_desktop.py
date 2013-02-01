e7c7b077a8ed52e3963d3db5f30d734699ae40a9
Kyle Miller <kyle.miller@asynchrony.com>
2013-02-01 10:41:25 -0600

    incremental commit, removed unneeded top padding widgets, only a single top padding widget now. kyle and srdjan [endlessm/eos-features-issues#851]

--- src/desktop/base_desktop.py
+++ src/desktop/base_desktop.py
@@ -5,41 +5,32 @@ class BaseDesktop(gtk.VBox):
     def __init__(self):
         super(BaseDesktop, self).__init__(False, 3)
 
-        self._top_taskbar_padding = None
+        self._top_page_padding_widget = None
         self._taskbar_widget = None
         self._searchbar_widget = None
-        self._top_searchbar_padding = None
         self._main_content_widget = None
-        self._top_page_buttons_padding_widget = None
         
     def get_main_content(self):
         return self._main_content_widget
     
     def set_taskbar_widget(self, taskbar_widget):
         self._remove_child(self._taskbar_widget)
-        self._remove_child(self._top_taskbar_padding)
+        self._remove_child(self._top_page_padding_widget)
         
         self._taskbar_widget = taskbar_widget
-        self._top_taskbar_padding = PaddingWidget()
+        self._top_page_padding_widget = PaddingWidget()
 
         self.pack_end(self._taskbar_widget, False, False, 0)
-        self.pack_start(self._top_taskbar_padding, False, False, 0)
+        self.pack_start(self._top_page_padding_widget, False, False, 0)
 
     def set_searchbar_widget(self, searchbar_widget):
         self._remove_child(self._searchbar_widget)
-        self._remove_child(self._top_searchbar_padding)
         
         self._searchbar_widget = searchbar_widget
-        self._top_searchbar_padding = PaddingWidget()
-
         self.pack_end(self._searchbar_widget, False, False, 0)
-        self.pack_start(self._top_searchbar_padding, False, False, 0)
 
     def set_page_buttons_widget(self, widget):
-        self._top_page_buttons_padding_widget = PaddingWidget()
         self._page_buttons_widget = widget
-
-        self.pack_start(self._top_page_buttons_padding_widget, False, False, 0)
         self.pack_end(self._page_buttons_widget, False, False, 0)
 
     def get_page_buttons_widget(self):
@@ -50,16 +41,13 @@ class BaseDesktop(gtk.VBox):
         self._searchbar_widget.set_size_request(0, middle_point)
 
         taskbar_width = self._taskbar_widget.allocation.width
+
         taskbar_height = self._taskbar_widget.allocation.height
-        self._top_taskbar_padding.set_size_request(taskbar_width, taskbar_height)
-
-        searchbar_width = self._searchbar_widget.allocation.width
         searchbar_height = self._searchbar_widget.size_request()[1]
-        self._top_searchbar_padding.set_size_request(searchbar_width, searchbar_height)
-
-        page_buttons_width = self._page_buttons_widget.allocation.width
         page_buttons_height = self._page_buttons_widget.size_request()[1] 
-        self._top_page_buttons_padding_widget.set_size_request(page_buttons_width, page_buttons_height)
+        total_top_padding = taskbar_height + searchbar_height + page_buttons_height
+
+        self._top_page_padding_widget.set_size_request(taskbar_width, total_top_padding)
 
     def set_main_content_widget(self, main_content_widget):
         self._remove_child(self._main_content_widget)
