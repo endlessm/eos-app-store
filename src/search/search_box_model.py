d46311e34ae5051cfc08c74406e081298ce2d92d
Matt Todd <matt.c.todd@gmail.com>
2013-02-13 16:14:28 -0600

    added a url validation to the model - Matt T & Matt I [endlessm/eos-features-issues#900]

--- src/search/search_box_model.py
+++ src/search/search_box_model.py
@@ -1,13 +1,21 @@
 from osapps.app_launcher import AppLauncher
+from url_validator import UrlValidator
 
 class SearchBoxModel():
     DEFAULT_URL = "www.google.com"
 
-    def __init__(self, app_launcher=AppLauncher()):
+    def __init__(self, app_launcher=AppLauncher(), url_validator=UrlValidator()):
         self._app_launcher = app_launcher
+        self._url_validator = url_validator
 
     def search(self, search_string):
-        if search_string is None or len(search_string) == 0:
+        if self._is_empty(search_string):
            self._app_launcher.launch_browser(self.DEFAULT_URL)
+        elif self._url_validator.validate(search_string):
+           self._app_launcher.launch_browser(search_string)
         else:
-            self._app_launcher.launch_search(search_string)
+           self._app_launcher.launch_search(search_string)
+
+    def _is_empty(self, text):
+        return not (text and len(text))
+
