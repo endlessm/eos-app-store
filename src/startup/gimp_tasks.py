720d84dbc85275254642d61db93e271a0c2f1a7b
Matt Todd <matt.c.todd@gmail.com>
2013-02-14 15:49:20 -0600

    ensure that the directory exists prior to copying the file over - Matt T & Matt I [endlessm/eos-features-issues#915]

--- src/startup/gimp_tasks.py
+++ src/startup/gimp_tasks.py
@@ -1,13 +1,19 @@
 import shutil
+import os
 from osapps.home_path_provider import HomePathProvider
 
 class GimpTasks():
     SOURCE = '/etc/endless-photo-editor/sessionrc'
+    TARGET_DIR = '.gimp-2.8'
     TARGET = '.gimp-2.8/sessionrc'
 
-    def __init__(self, copier=shutil.copy, home_path_provider=HomePathProvider()):
+    def __init__(self, copier=shutil.copy, directory_creator=os.makedirs, home_path_provider=HomePathProvider()):
         self._copier = copier
         self._home_path_provider = home_path_provider
+        self._directory_creator = directory_creator
 
     def execute(self):
-        self._copier(self.SOURCE, self._home_path_provider.get_user_directory(self.TARGET))
+        destination_file = self._home_path_provider.get_user_directory(self.TARGET)
+        destination_directory = self._home_path_provider.get_user_directory(self.TARGET_DIR)
+        self._directory_creator(destination_directory)
+        self._copier(self.SOURCE, destination_file)
--- test/startup_test/gimp_tasks_test.py
+++ test/startup_test/gimp_tasks_test.py
@@ -1,25 +1,32 @@
 import unittest
-from mock import Mock
+from mock import Mock, call
 
 from startup.gimp_tasks import GimpTasks
 
 class GimpTasksTest(unittest.TestCase):
     def setUp(self):
-        mock_manager = Mock()
-        self.copier = mock_manager.copier
-        self.home_path_provider = mock_manager.home_path_provider
-        self._test_object = GimpTasks(self.copier, self.home_path_provider)
+        self.mock_manager = Mock()
+        self.directory_creator = self.mock_manager.directory_creator
+        self.copy = self.mock_manager.copy
+        self.home_path_provider = Mock()
+        self._test_object = GimpTasks(self.copy, self.directory_creator, self.home_path_provider)
 
     def test_copies_sessionrc_to_users_home(self):
-        destination = "destination"
-        self.home_path_provider.get_user_directory = Mock(return_value=destination)
+        destination_file = "destination file"
+        destination_directory = "destination dir"
+
+        def get_user_dir_side_effect(*args, **kwargs):
+            if args[0] == self._test_object.TARGET:
+                return destination_file
+            elif args[0] == self._test_object.TARGET_DIR:
+                return destination_directory
+        self.home_path_provider.get_user_directory = Mock(side_effect=get_user_dir_side_effect)
 
         self._test_object.execute()
 
-        self.copier.assert_called_once_with(self._test_object.SOURCE, destination)
-
-    def test_get_user_directory_called_with_target_filename(self):
-        self._test_object.execute()
-
-        self.home_path_provider.get_user_directory.assert_called_once_with(self._test_object.TARGET)
+        expected_calls = [
+            call.directory_creator(destination_directory),
+            call.copy(self._test_object.SOURCE, destination_file)
+            ]
+        self.assertEquals(self.mock_manager.mock_calls, expected_calls)
 
