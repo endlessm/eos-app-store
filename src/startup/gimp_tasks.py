b1a90499fdbaf4ef2a570f0343333426dd272848
Matt Todd <matt.c.todd@gmail.com>
2013-02-14 16:09:20 -0600

    now using a directory creator that doens't suck - Matt T & Matt I [endlessm/eos-features-issues#915]

--- src/startup/gimp_tasks.py
+++ src/startup/gimp_tasks.py
@@ -1,5 +1,5 @@
 import shutil
-import os
+from distutils import dir_util
 from osapps.home_path_provider import HomePathProvider
 
 class GimpTasks():
@@ -7,7 +7,7 @@ class GimpTasks():
     TARGET_DIR = '.gimp-2.8'
     TARGET = '.gimp-2.8/sessionrc'
 
-    def __init__(self, copier=shutil.copy, directory_creator=os.makedirs, home_path_provider=HomePathProvider()):
+    def __init__(self, copier=shutil.copy, directory_creator=dir_util.mkpath, home_path_provider=HomePathProvider()):
         self._copier = copier
         self._home_path_provider = home_path_provider
         self._directory_creator = directory_creator
