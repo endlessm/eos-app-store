2cc3d614288dad1f6981133ef98dc2f2148a8fee
Matt Todd <matt.c.todd@gmail.com>
2013-02-21 11:56:52 -0600

    made a few changes to ensure that the tests don't need X for dbus - Matt T & Srjan & Erik [endlessm/eos-features-issues#861b]

--- src/notification_panel/battery_plugin.py
+++ src/notification_panel/battery_plugin.py
@@ -6,15 +6,15 @@ from battery_provider import BatteryProvider
 
 class BatteryPlugin(NotificationPlugin):
     def __init__(self, icon_size):
-        super(BatteryPlugin, self).__init__(icon_size)
+        super(BatteryPlugin, self).__init__(None)
         self._presenter = BatteryPresenter(BatteryView(self, icon_size), BatteryModel())
 
     def post_init(self):
         self._presenter.post_init()
-    
+
     def execute(self):
         self._presenter.display_menu() 
 
     @staticmethod
-    def is_plugin_enabled():
-        return BatteryProvider().get_battery_info().level() is not None
+    def is_plugin_enabled(battery_provider=BatteryProvider()):
+        return battery_provider.get_battery_info().level() is not None
--- test/notification_panel_test/test_battery_plugin.py
+++ test/notification_panel_test/test_battery_plugin.py
@@ -2,19 +2,22 @@ import unittest
 from mock import Mock
 
 from notification_panel.battery_plugin import BatteryPlugin
-from util.dbus_utils import DbusUtils
 
 class BatteryPluginTestCase(unittest.TestCase):
+    def setUp(self):
+        self.battery_provider = Mock()
+        self.battery_info = Mock()
+        self.battery_provider.get_battery_info = Mock(return_value=self.battery_info)
     
     def test_battery_plugin_disabled_if_no_battery_present(self):
-        DbusUtils.has_device_of_type = Mock(return_value = False)
-        
-        self.assertFalse(BatteryPlugin.is_plugin_enabled())
+        self.battery_info.level = Mock(return_value=None)
+
+        self.assertFalse(BatteryPlugin.is_plugin_enabled(self.battery_provider))
         
     def test_battery_plugin_enabled_if_battery_present(self):
-        DbusUtils.has_device_of_type = Mock(return_value = True)
+        self.battery_info.level = Mock(return_value="yeah!")
         
-        self.assertTrue(BatteryPlugin.is_plugin_enabled())
+        self.assertTrue(BatteryPlugin.is_plugin_enabled(self.battery_provider))
 
     def test_execute_calls_display_menu(self):
         test_object = BatteryPlugin(22)
