62afe4bb0232861404fb6a3a3bed0fd00949a162
Matt Todd <matt.c.todd@gmail.com>
2013-03-22 08:55:43 -0500

    fix to the build - Matt T & Srdjan

--- src/search/search_box_model.py
+++ src/search/search_box_model.py
@@ -1,20 +1,20 @@
-from osapps.app_launcher import AppLauncher
+from osapps.browser_launcher import BrowserLauncher
 from url_validator import UrlValidator
 
 class SearchBoxModel():
     DEFAULT_URL = "www.google.com"
 
-    def __init__(self, app_launcher=AppLauncher(), url_validator=UrlValidator()):
-        self._app_launcher = app_launcher
+    def __init__(self, browser_launcher=BrowserLauncher, url_validator=UrlValidator()):
+        self._browser_launcher = browser_launcher()
         self._url_validator = url_validator
 
     def search(self, search_string):
         if self._is_empty(search_string):
-           self._app_launcher.launch_browser(self.DEFAULT_URL)
+           self._browser_launcher.launch_browser(self.DEFAULT_URL)
         elif self._url_validator.validate(search_string):
-           self._app_launcher.launch_browser(search_string)
+           self._browser_launcher.launch_browser(search_string)
         else:
-           self._app_launcher.launch_search(search_string)
+           self._browser_launcher.launch_search(search_string)
 
     def _is_empty(self, text):
         return not (text and len(text))
--- src/taskbar_panel/browser_button.py
+++ src/taskbar_panel/browser_button.py
@@ -1,12 +1,12 @@
 from browser_button_presenter import BrowserButtonPresenter
 from browser_button_view import BrowserButtonView
 from browser_button_model import BrowserButtonModel
-from osapps.app_launcher import AppLauncher
+from osapps.browser_launcher import BrowserLauncher
 
 class BrowserButton():
    def __init__(self):
       self._button = BrowserButtonView()
-      BrowserButtonPresenter(self._button, BrowserButtonModel(), AppLauncher())
+      BrowserButtonPresenter(self._button, BrowserButtonModel(), BrowserLauncher())
 
    def get_button(self):
       return self._button
