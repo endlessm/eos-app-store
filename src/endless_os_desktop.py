b03c73853d19d170c22c8161a1d635225a0c5fb3
Erik Wolfe <erik.wolfe@asynchrony.com>
2013-04-02 16:02:58 -0500

    Stripping out extra desktop code.  Working towards a the app store being it's own app. Erik

--- src/endless_os_desktop.py
+++ src/endless_os_desktop.py
@@ -3,12 +3,8 @@ import gtk
 import gobject
 
 from desktop_builder import build_desktop
-from startup.initial_tasks import InitialTasks
-from startup.startup_tasks import StartupTasks
 
 if __name__ == "__main__":
-   InitialTasks().perform_tasks()
-   StartupTasks().perform_tasks()
 
    build_desktop()
 
