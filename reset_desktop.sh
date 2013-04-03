6417e53493892daeb19da3d1cd0747125b1c4a3f
Erik Wolfe <erik.wolfe@asynchrony.com>
2013-04-03 15:44:57 -0500

    Removing extra scripts. Erik and Kyle
    [endlessm/eos-shell#64]

--- reset_desktop.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm ~/.endlessm/desktop.json
--- restart-launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eos_desktop_dir=/home/endlessm/checkout/eos-desktop
-
-pushd ${eos_desktop_dir}
-  nohup ./restart.sh > console-start.log 2>&1 &
-popd
-
-sleep 1
