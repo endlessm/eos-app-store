cc8205cc53589e15e54e4c042912fc818c8f5bc0
Erik Wolfe <erik.wolfe@asynchrony.com>
2013-04-03 10:02:23 -0500

    Removing an unused import. Erik

--- src/desktop/endless_desktop_view.py
+++ src/desktop/endless_desktop_view.py
@@ -14,18 +14,12 @@ from folder.folder_window import FULL_FOLDER_ITEMS_COUNT
 from add_shortcuts_module.add_shortcuts_view import AddShortcutsView
 from eos_util.image import Image
 from shortcut.desktop_shortcut import DesktopShortcut
-from desktop_page.desktop_page_view import DesktopPageView
-from search.search_box import SearchBox
-from desktop.base_desktop import BaseDesktop
-from desktop_nav_button import DesktopNavButton
 from ui.padding_widget import PaddingWidget
-from desktop_page.button import Button
 
 gettext.install('endless_desktop', '/usr/share/locale', unicode=True, names=['ngettext'])
 gtk.gdk.threads_init()
 
 class EndlessDesktopView(gtk.Window, object):
-    _app_shortcuts = {}
 
     def __init__(self):
         gtk.Window.__init__(self)
@@ -46,8 +40,4 @@ class EndlessDesktopView(gtk.Window, object):
         height = geometry.height
 
         return width, height
-
-    def show_add_dialogue(self, event_box, event):
-        add_shortcut_popup = AddShortcutsView(parent=self)
-        add_shortcut_popup.show()
 
