e59d9cad0c3446bf022d47e4c12c466a6471d471
Juan A. Suarez Romero <jasuarez@igalia.com>
2013-05-16 16:47:41 +0200

    Run only one instance of application
    [endlessm/eos-shell#64]

--- src/app_store/eos_app_store.py
+++ src/app_store/eos_app_store.py
@@ -7,11 +7,13 @@ from add_shortcuts_module.add_shortcuts_view import AddShortcutsView
 gettext.install('eos_app_store', '/usr/share/locale', unicode=True, names=['ngettext'])
 class EosAppStore(Gtk.Application):
     def __init__(self):
-        Gtk.Application.__init__(self)
+        Gtk.Application.__init__(self, application_id="com.endlessm.eos_app_store")
+        self._win = None
 
     def do_activate(self):
-        win = AddShortcutsView(self)
-        win.show_all()
+        if not self._win:
+            self._win = AddShortcutsView(self)
+            self._win.show_all()
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
