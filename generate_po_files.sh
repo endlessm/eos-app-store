9a2d69d751bec5444cd9b3b9dd43732986dad8d0
Roddy Shuler <roddy@endlessm.com>
2013-07-22 07:49:08 -0700

    Complete autotools gettext support
    The framework was mostly in place but not quite complete,
    and we were still using old scripts to generate the translation files.
    The missing pieces have been added, and the old scripts removed.
    Note that "make check" autogenerates the .pot file,
    so the .pot file can also be removed from the repo.
    
    [endlessm/eos-shell#532]

--- generate_po_files.sh
+++
@@ -1,2 +0,0 @@
-msginit -i po/endless_template.pot -l pt --no-translator -o po/pt.po
-msginit -i po/endless_template.pot -l es --no-translator -o po/es.po
--- merge_pot_file_with_po_files.sh
+++
@@ -1,2 +0,0 @@
-msgmerge po/pt.po po/endless_template.pot -o po/pt.po
-msgmerge po/es.po po/endless_template.pot -o po/es.po
