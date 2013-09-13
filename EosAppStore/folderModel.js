dde19a8d803b0d8ab49003f763204c2a35513751
Felipe Erias Morandeira <femorandeira@igalia.com>
2013-09-13 16:10:35 +0100

    Write the entry for the new directory to a new file in .local/share/desktop-directories/
    [endlessm/eos-shell#323]

--- EosAppStore/folderModel.js
+++ EosAppStore/folderModel.js
@@ -22,6 +22,37 @@ function getIconList() {
     return folderIcons;
 };
 
-function createFolder(label, iconName) {
-    print('Add folder '+label+' with icon '+iconName);
+function createFolder(name, iconName) {
+    let keyFile = new GLib.KeyFile();
+    
+    keyFile.set_value(GLib.KEY_FILE_DESKTOP_GROUP,
+                     GLib.KEY_FILE_DESKTOP_KEY_NAME, name);
+    
+    keyFile.set_value(GLib.KEY_FILE_DESKTOP_GROUP,
+                     GLib.KEY_FILE_DESKTOP_KEY_ICON, iconName);
+    
+    keyFile.set_value(GLib.KEY_FILE_DESKTOP_GROUP,
+                     GLib.KEY_FILE_DESKTOP_KEY_TYPE, GLib.KEY_FILE_DESKTOP_TYPE_DIRECTORY);
+    
+    let dir = GLib.get_user_data_dir()+'/desktop-directories';
+    
+    // apparently, octal literals are deprecated in JS
+    if (GLib.mkdir_with_parents(dir, 0755, null) < 0) {
+        log('could not create the directory '+dir);
+        return;
+    }
+
+    //let fd = GLib.mkstemp_full(dir + '/eos-folder-user-XXXXXX.directory', O_RDWR, 0664);
+    let fd = GLib.mkstemp(dir + '/eos-folder-user-XXXXXX.directory');
+
+    if (fd < 0) {
+        log('could not create a new file in '+dir);
+        return;
+    }
+    
+    let buf = keyFile.to_data();
+    
+    let channel = GLib.IOChannel.unix_new(fd);
+    channel.write_chars(buf[0], buf[1]);
+    channel.shutdown(true);
 };
