03dc5cb8a776a479e9863e446fa79b402e98a30c
Felipe Erias Morandeira <femorandeira@igalia.com>
2013-09-16 15:59:08 +0100

    Directory files are of the form eos-folder-user-N.directory, where N is a natural number.
    [endlessm/eos-shell#323]

--- EosAppStore/folderModel.js
+++ EosAppStore/folderModel.js
@@ -34,25 +34,39 @@ function createFolder(name, iconName) {
     keyFile.set_value(GLib.KEY_FILE_DESKTOP_GROUP,
                      GLib.KEY_FILE_DESKTOP_KEY_TYPE, GLib.KEY_FILE_DESKTOP_TYPE_DIRECTORY);
 
-    let dir = GLib.get_user_data_dir()+'/desktop-directories';
+    let dirpath = GLib.get_user_data_dir()+'/desktop-directories';
 
     // apparently, octal literals are deprecated in JS
-    if (GLib.mkdir_with_parents(dir, 0755, null) < 0) {
-        log('could not create the directory '+dir);
+    if (GLib.mkdir_with_parents(dirpath, 0755, null) < 0) {
+        log('could not create the directory '+dirpath);
         return;
     }
 
-    //let fd = GLib.mkstemp_full(dir + '/eos-folder-user-XXXXXX.directory', O_RDWR, 0664);
-    let fd = GLib.mkstemp(dir + '/eos-folder-user-XXXXXX.directory');
+    let dir =  Gio.File.new_for_path(dirpath);
 
-    if (fd < 0) {
-        log('could not create a new file in '+dir);
-        return;
+    let enumerator = dir.enumerate_children('standard::name',
+                                            Gio.FileQueryInfoFlags.NONE, null);
+
+    let prefix = 'eos-folder-user-';
+    let suffix = '.directory';
+    let re = new RegExp(prefix + '[0-9]+' + suffix);
+    let n = -1;
+
+    for (let f = enumerator.next_file(null); f != null; f = enumerator.next_file(null)) {
+        let name = f.get_name();
+        if (re.test(name)) {
+            let new_n = name.slice(name.indexOf(prefix) + prefix.length,
+                                   name.indexOf(suffix));
+            n = Math.max(n, parseInt(new_n));
+        }
     }
+    enumerator.close(null);
+
+    let filename = prefix + (n+1) + suffix;
 
     let buf = keyFile.to_data();
 
-    let channel = GLib.IOChannel.unix_new(fd);
+    let channel = GLib.IOChannel.new_file(dirpath + '/' + filename, 'w');
     channel.write_chars(buf[0], buf[1]);
     channel.shutdown(true);
 };
