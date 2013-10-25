ac790e24c8258b504e883d7f706a618790bc5175
Cosimo Cecchi <cosimo@endlessm.com>
2013-10-25 16:06:39 -0700

    Filter out applications that are not available
    If an application is not available in a specific image, filter it out
    from the app store.
    
    [endlessm/eos-shell#839]

--- EosAppStore/appFrame.js
+++ EosAppStore/appFrame.js
@@ -307,6 +307,7 @@ const AppFrame = new Lang.Class({
 
     _populateCategories: function() {
         let cellMargin = EosAppStorePrivate.AppInfo.get_cell_margin();
+        let availableApps = this._model.apps;
 
         for (let c in this._categories) {
             let category = this._categories[c];
@@ -344,15 +345,18 @@ const AppFrame = new Lang.Class({
             scrollWindow.add_with_viewport(grid);
 
             let appInfos = EosAppStorePrivate.app_load_content(category.id);
-            if (category.id == EosAppStorePrivate.AppCategory.MY_APPLICATIONS) {
-                let installedApps = this._model.apps;
+            appInfos = appInfos.filter(function(appInfo) {
+                let id = appInfo.get_desktop_id() + '.desktop';
+                return (availableApps.indexOf(id) != -1);
+            });
 
+            if (category.id == EosAppStorePrivate.AppCategory.MY_APPLICATIONS) {
                 for (let i in appInfos) {
                     let id = appInfos[i].get_desktop_id() + '.desktop';
 
                     let isInstalled = false;
-                    for (let j in installedApps) {
-                        if (installedApps[j] == id &&
+                    for (let j in availableApps) {
+                        if (availableApps[j] == id &&
                             this._model.getState(id) == EosAppStorePrivate.AppState.INSTALLED) {
                             isInstalled = true;
                             break;
