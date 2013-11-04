6bc6c81f0fa038fcfd8968848d8d75b0f44c2b8b
Roddy Shuler <roddy@endlessm.com>
2013-11-04 15:59:11 -0800

    Load content gresources from new location
    These resources are now installed into /usr/share/eos-shell-content.
    
    [endlessm/eos-shell#1346]

--- EosAppStore/appStore.js
+++ EosAppStore/appStore.js
@@ -85,12 +85,13 @@ const AppStore = new Lang.Class({
     },
 
     _loadResources: function() {
-        let resources = ['eos-app-store.gresource',
-                         'eos-app-store-app-content.gresource',
-                         'eos-app-store-link-content.gresource'];
+        let resources =
+            [[Path.RESOURCE_DIR, 'eos-app-store.gresource'],
+             [Path.CONTENT_DIR, 'eos-app-store-app-content.gresource'],
+             [Path.CONTENT_DIR, 'eos-app-store-link-content.gresource']];
 
         for (let idx in resources) {
-            let path = GLib.build_filenamev([Path.RESOURCE_DIR, resources[idx]]);
+            let path = GLib.build_filenamev(resources[idx]);
             let resource = Gio.Resource.load(path);
             resource._register();
         }
