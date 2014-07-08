f674e7d2e18592b24797beeeae174b4813898638
Emmanuele Bassi <ebassi@gnome.org>
2014-07-08 16:38:03 +0100

    Hide app-manager proxy object
    [endlessm/eos-shell#2661]

--- EosAppStore/appManager.js
+++ EosAppStore/appManager.js
@@ -21,7 +21,15 @@ const AppManager = new Lang.Class({
     Name: 'AppManager',
 
     _init: function() {
-        this.proxy = new AppManagerProxy(Gio.DBus.system,
-            APP_MANAGER_NAME, APP_MANAGER_PATH);
+        this._proxy = new AppManagerProxy(Gio.DBus.system,
+                                          APP_MANAGER_NAME,
+                                          APP_MANAGER_PATH);
+    },
+
+    refresh: function() {
+        // refresh all applications; ListAvailable() will filter
+        // them later on inside the EosAppListModel
+        let opts = {};
+        this._proxy.RefreshRemote(opts);
     }
 });
