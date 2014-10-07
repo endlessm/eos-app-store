84294ddd96a4cdcac8af1a1ef3972eda381be46e
Emmanuele Bassi <ebassi@gnome.org>
2014-10-07 17:22:40 +0100

    Download bundle files under /var/tmp
    It's a globally writable directory, and it's not on tmpfs.
    
    [endlessm/eos-shell#3822]

--- EosAppStore/lib/eos-app-list-model.c
+++ EosAppStore/lib/eos-app-list-model.c
@@ -822,7 +822,7 @@ download_bundle_from_uri (EosAppListModel *self,
   goffset total = soup_request_get_content_length (request);
 
   char *bundle_file = g_strconcat (app_id, ".bundle", NULL);
-  char *target = g_build_filename (g_get_tmp_dir (), "eos-app-store", bundle_file, NULL);
+  char *target = g_build_filename (BUNDLEDIR, bundle_file, NULL);
   g_free (bundle_file);
 
   GFile *file = g_file_new_for_path (target);
