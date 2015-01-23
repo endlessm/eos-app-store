0dda5113b9fbfb1999c4ccc64b2f0244110093cd
Srdjan Grubor <sgnn7@sgnn7.org>
2015-01-23 12:18:06 -0600

    Added a journal logger class to help with debugging
    This should provide us with better dynamic debugging when problems are
    in the C library by sending messages to systemd journal.

---
+++ EosAppStore/lib/eos-app-log.c
@@ -0,0 +1,85 @@
+/* Copyright 2015 Endless Mobile, Inc. */
+#include "eos-app-log.h"
+
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <systemd/sd-journal.h>
+
+static void
+eos_app_log_messagev (int level,
+                  const char *fmt,
+                  va_list args)
+{
+  static const char *eos_app_testing;
+
+  if (eos_app_testing == NULL) {
+    const char *env = g_getenv ("EAS_TESTING");
+    if (env != NULL && *env == '1')
+      eos_app_testing = "1";
+    else
+      eos_app_testing = "0";
+  }
+
+  /* if we're not under a test environment, then we send
+   * everything to the journal
+   */
+  if (*eos_app_testing == '0') {
+    sd_journal_printv (level, fmt, args);
+    return;
+  }
+
+  switch (level) {
+    case LOG_DEBUG:
+      g_logv (G_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, fmt, args);
+      return;
+
+    case LOG_INFO:
+      g_logv (G_LOG_DOMAIN, G_LOG_LEVEL_INFO, fmt, args);
+      return;
+
+    default:
+      vfprintf (stderr, fmt, args);
+      fputc ('\n', stderr);
+      break;
+  }
+}
+
+void
+eos_app_log_debug_message (const char *fmt,
+                       ...)
+{
+  va_list args;
+
+  va_start (args, fmt);
+
+  eos_app_log_messagev (LOG_DEBUG, fmt, args);
+
+  va_end (args);
+}
+
+void
+eos_app_log_error_message (const char *fmt,
+                       ...)
+{
+  va_list args;
+
+  va_start (args, fmt);
+
+  eos_app_log_messagev (LOG_ERR, fmt, args);
+
+  va_end (args);
+}
+
+void
+eos_app_log_info_message (const char *fmt,
+                      ...)
+{
+  va_list args;
+
+  va_start (args, fmt);
+
+  eos_app_log_messagev (LOG_INFO, fmt, args);
+
+  va_end (args);
+}
