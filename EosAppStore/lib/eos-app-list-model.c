76e8108247e81a1b85f5a32461b3d54bd2b4d0ba
Srdjan Grubor <sgnn7@sgnn7.org>
2015-02-13 10:00:22 -0600

    Added ability to retry downloading of files for a short time
    This change adds retry functionality to downloads so that if there's a
    download failure, the logic will retry every X seconds for a maximum of
    Y seconds. If any data is able to be retrieved, the retry counters are
    reset so that we don't consider most temp interruptions in a global
    context.
    
    For now, X = 20 and Y = 4

--- EosAppStore/lib/eos-app-list-model.c
+++ EosAppStore/lib/eos-app-list-model.c
@@ -1059,7 +1059,8 @@ download_file_from_uri (EosAppListModel *self,
                         ProgressReportFunc progress_func,
                         gpointer         progress_func_user_data,
                         GCancellable    *cancellable,
-                        GError         **error)
+                        GError         **error,
+                        gboolean        *reset_error_counter)
 {
   GError *internal_error = NULL;
   gboolean retval = FALSE;
@@ -1212,6 +1213,9 @@ download_file_from_uri (EosAppListModel *self,
 
       pos += res;
 
+      /* Since we got some data, we can assume that network is back online */
+      *reset_error_counter = TRUE;
+
       if (progress_func != NULL)
         progress_func (app_id, pos, total, progress_func_user_data);
     }
@@ -1267,16 +1271,78 @@ download_file_from_uri_with_retry (EosAppListModel *self,
                                    ProgressReportFunc progress_func,
                                    gpointer         progress_func_user_data,
                                    GCancellable    *cancellable,
-                                   GError         **error)
+                                   GError         **error_out)
 {
-    return download_file_from_uri (self, app_id, source_uri, target_file,
-                                   progress_func,
-                                   progress_func_user_data,
-                                   cancellable,
-                                   error);
+    gboolean download_success = FALSE;
+    gboolean reset_error_counter = FALSE;
+
+    GError *error = NULL;
+
+    gint64 retry_time_limit = 20 * G_USEC_PER_SEC;
+
+    gint64 error_retry_start = 0;
+    gint64 error_retry_cutoff = 0;
+
+    /* Keep trying to donwload unless we finish successfully or we reach
+     * the retry timeout
+     */
+    while (TRUE) {
+        download_success = download_file_from_uri (self, app_id, source_uri,
+                                                   target_file,
+                                                   progress_func,
+                                                   progress_func_user_data,
+                                                   cancellable,
+                                                   &error,
+                                                   &reset_error_counter);
+
+        /* If there's an error, we didn't successfully download the file either */
+        download_success = download_success && (error == NULL);
+
+        /* We're done if we get the file or we're canceled so exit the loop */
+        if (download_success || g_cancellable_is_cancelled(cancellable))
+            break;
+
+        eos_app_log_error_message ("Error downloading. Checking if retries are needed");
+
+        if (reset_error_counter) {
+            eos_app_log_info_message ("Some data retrieved during download failure. "
+                                      "Resetting retry timeouts.");
+            error_retry_start = 0;
+            error_retry_cutoff = 0;
+        }
+
+        /* If this is our first retry, record the start time */
+        if (error_retry_start == 0) {
+            error_retry_start = g_get_monotonic_time ();
+            error_retry_cutoff = error_retry_start + retry_time_limit;
+        }
+
+        /* If we reached our limit of retry time, exit */
+        if (g_get_monotonic_time () >= error_retry_cutoff) {
+            eos_app_log_error_message ("Retry limit reached. Exiting with failure");
+
+            g_propagate_error (error_out, error);
+
+            break;
+        }
+
+        /* Ignore the error if we need to run again */
+        g_clear_error(&error);
+
+        eos_app_log_error_message ("Retrying to download the file after a short break");
+
+        /* Reset the progress position to beginning */
+        if (progress_func != NULL)
+            progress_func (app_id, 0, 0, progress_func_user_data);
+
+        /* Sleep for 4 seconds and try again */
+        g_usleep(4 * G_USEC_PER_SEC);
+
+        eos_app_log_error_message ("Continuing download loop...");
+    }
+
+    return download_success;
 }
-
-
 
 static char *
 create_sha256sum (EosAppListModel *self,
