6e3932d8176ec5192d7c663cb9bfaec2a6e31eed
Srdjan Grubor <sgnn7@sgnn7.org>
2015-03-20 10:21:30 -0500

    Added config module to the app store libraries
    We will need a separate module to do yank files out of the configuration
    since even a basic initialization fo those files is gruesome.

---
+++ EosAppStore/lib/eos-app-config.c
@@ -0,0 +1,12 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+#include <glib.h>
+
+#include "config.h"
+#include "eos-app-config.h"
+
+const gchar *
+eos_app_config_get_cache_dir (void)
+{
+  return NULL;
+}
---
+++ EosAppStore/lib/eos-app-config.h
@@ -0,0 +1,14 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+
+#ifndef EOS_APP_CONFIG_H
+#define EOS_APP_CONFIG_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+const gchar *eos_app_config_get_cache_dir (void);
+
+G_END_DECLS
+
+#endif /* EOS_APP_CONFIG_H */
