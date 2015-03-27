2acb337b00c2e21b25ed71ae5f87802787a4d1eb
Cosimo Cecchi <cosimo@endlessm.com>
2015-03-27 15:49:11 -0700

    app-list-model: use better debug messages for upgrades
    [endlessm/eos-shell#4465]

--- EosAppStore/lib/eos-app-list-model.c
+++ EosAppStore/lib/eos-app-list-model.c
@@ -1731,19 +1731,23 @@ install_latest_app_version (EosAppListModel *self,
   char *app_id = app_id_from_desktop_id (desktop_id);
   char *transaction_path = NULL;
 
-  eos_app_log_info_message ("Calling install dbus method with app_id: %s", app_id);
-
   /* We use different DBus targets but everything else is same */
   if (is_upgrade)
-    eos_app_manager_call_update_sync (proxy, app_id, allow_deltas,
-                                      &transaction_path,
-                                      NULL,
-                                      &error);
+    {
+      eos_app_log_info_message ("Calling update dbus method with app_id: %s", app_id);
+      eos_app_manager_call_update_sync (proxy, app_id, allow_deltas,
+                                        &transaction_path,
+                                        NULL,
+                                        &error);
+    }
   else
-    eos_app_manager_call_install_sync (proxy, app_id,
-                                       &transaction_path,
-                                       NULL,
-                                       &error);
+    {
+      eos_app_log_info_message ("Calling install dbus method with app_id: %s", app_id);
+      eos_app_manager_call_install_sync (proxy, app_id,
+                                         &transaction_path,
+                                         NULL,
+                                         &error);
+    }
 
   g_free (app_id);
 
