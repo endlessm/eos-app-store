e821f3b0db7b8023bb445546c47a1c4b0b2eeae9
Srdjan Grubor <sgnn7@sgnn7.org>
2015-05-15 12:52:32 -0500

    Initial add of the delta updates tool
    Doesn't do anything yet but that will change soon

---
+++ tools/delta_updates_tool.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class DeltaUpdatesTool(object):
+    def trim(self):
+        # TODO: Implement me
+        pass
+
+if __name__ == '__main__':
+    DeltaUpdatesTool().trim()
