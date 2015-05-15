65f88f308b81b430504d9f6baedf10ef13a61982
Srdjan Grubor <sgnn7@sgnn7.org>
2015-05-15 12:59:03 -0500

    Fleshed out the basic skeleton of the delta updates tool
    Really really basic and doesn't do anything for now but it should soon
    start to look a bit more usable.

--- tools/delta_updates_tool.py
+++ tools/delta_updates_tool.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 
 class DeltaUpdatesTool(object):
-    def trim(self):
-        # TODO: Implement me
+    def parse_updates_json(self, location):
         pass
+
+    def trim_newer_full_updates(self, unfiltered_updates):
+        return unfiltered_updates
+
+    def save_json(self, location, data):
+        pass
+
+    def trim(self):
+        location = './updates.json'
+
+        actual_updates = self.parse_updates_json(location)
+        filtered_updates = self.trim_newer_full_updates(actual_updates)
+        self.save_json(location, filtered_updates)
 
 if __name__ == '__main__':
     DeltaUpdatesTool().trim()
