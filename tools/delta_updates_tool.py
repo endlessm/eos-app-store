ea8bd6e81c47712b07254a699734d23453c0983f
Srdjan Grubor <sgnn7@sgnn7.org>
2015-05-15 13:08:00 -0500

    Added parsing of json functionality to delta updates tool
    Now the tool can read JSON but right not it doesn't do anything with it.

--- tools/delta_updates_tool.py
+++ tools/delta_updates_tool.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+import json
+
+from os import path
+
 class DeltaUpdatesTool(object):
     def parse_updates_json(self, location):
-        pass
+        if not path.exists(location):
+            raise RuntimeError("File does not exist at %s" % location)
+
+        data = None
+        with open(location) as json_file:
+            data = json.load(json_file)
+
+        if not data:
+            raise RuntimeError("No valid JSON data found in %s" % location)
+
+        print("Found %d records in file" % len(data))
+
+        return data
 
     def trim_newer_full_updates(self, unfiltered_updates):
         return unfiltered_updates
@@ -12,10 +28,11 @@ class DeltaUpdatesTool(object):
 
     def trim(self):
         location = './updates.json'
+        output_location = './updates.new.json'
 
         actual_updates = self.parse_updates_json(location)
         filtered_updates = self.trim_newer_full_updates(actual_updates)
-        self.save_json(location, filtered_updates)
+        self.save_json(output_location, filtered_updates)
 
 if __name__ == '__main__':
     DeltaUpdatesTool().trim()
