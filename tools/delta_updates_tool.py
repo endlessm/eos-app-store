e3856a2987d9ef638e72145151575ad3d4a72780
Srdjan Grubor <sgnn7@sgnn7.org>
2015-05-18 12:00:39 -0500

    Added filtering of irrelevant updates in delta updates tool
    This should leave only relevant updates available in the updates json
    though there are still issues of changing the mtime of
    updates/updates_meta and items with erroneous one update remaining in
    the resulting set (the latter being tackled next).

--- tools/delta_updates_tool.py
+++ tools/delta_updates_tool.py
@@ -47,35 +47,54 @@ class DeltaUpdatesTool(object):
 
         return bucket_name
 
-    def _compare_versions(self, a, b, cmp_func):
+    def _compare_code_versions(self, a, b, cmp_func):
         return call('dpkg --compare-versions %s %s %s' % (a['codeVersion'],
                                                           cmp_func,
                                                           b['codeVersion']),
                     shell = True)
 
-    def _sort_by_versions(self, updates):
-        def cmp_to_key(mycmp):
-            'Convert cmp= function to key= function'
-            class KeyComparator(object):
-                def __init__(self, obj, *args):
-                    self.obj = obj
-                def __lt__(self, other):
-                    return mycmp(self.obj, other.obj, 'lt')
-                def __gt__(self, other):
-                    return mycmp(self.obj, other.obj, 'gt')
-                def __eq__(self, other):
-                    return mycmp(self.obj, other.obj, 'eq')
-                def __le__(self, other):
-                    return mycmp(self.obj, other.obj, 'lte')
-                def __ge__(self, other):
-                    return mycmp(self.obj, other.obj, 'gte')
-                def __ne__(self, other):
-                    return mycmp(self.obj, other.obj, 'ne')
-            return KeyComparator
+    def _compare_from_versions(self, a, b, cmp_func):
+        return call('dpkg --compare-versions %s %s %s' % (a['fromVersion'],
+                                                          cmp_func,
+                                                          b['fromVersion']),
+                    shell = True)
 
-        return sorted(updates, key=cmp_to_key(self._compare_versions))
+    def cmp_to_key(self, comparator):
+        'Convert cmp= function to key= function'
+        class KeyComparator(object):
+            def __init__(self, obj, *args):
+                self.obj = obj
+            def __lt__(self, other):
+                return comparator(self.obj, other.obj, 'lt')
+            def __gt__(self, other):
+                return comparator(self.obj, other.obj, 'gt')
+            def __eq__(self, other):
+                return comparator(self.obj, other.obj, 'eq')
+            def __le__(self, other):
+                return comparator(self.obj, other.obj, 'lte')
+            def __ge__(self, other):
+                return comparator(self.obj, other.obj, 'gte')
+            def __ne__(self, other):
+                return comparator(self.obj, other.obj, 'ne')
+        return KeyComparator
+
+    def _sort_by_code_versions(self, updates):
+        return sorted(updates, key=self.cmp_to_key(self._compare_code_versions))
+
+    def _split_newer_updates(self, deltas):
+        sorted_deltas = sorted(deltas, key=self.cmp_to_key(self._compare_from_versions))
+        print("Sorted: ", len(sorted_deltas))
+
+        if len(sorted_deltas) == 1:
+            return [], sorted_deltas[-1]
+
+        return sorted_deltas[:-2], sorted_deltas[-1]
 
     def trim_newer_full_updates(self, unfiltered_updates):
+        filtered_updates = []
+
+        # Split unfiltered updates into buckets of installable app
+        # groupings
         buckets = {}
         for update in unfiltered_updates:
             bucket_name = self._build_app_bucket_name(update)
@@ -88,10 +107,49 @@ class DeltaUpdatesTool(object):
 
         print("Assembled %s buckets of updates" % len(buckets.keys()))
 
+        # Filter each bucketted update group
         for bucket_name, updates in buckets.items():
             print(bucket_name, len(updates))
 
-            sorted_updates = self._sort_by_versions(updates)
+            sorted_updates = self._sort_by_code_versions(updates)
+
+            latest_version = sorted_updates[0]['codeVersion']
+            # print('Latest: %s' % latest_version)
+
+            latest_version_updates = [u for u in sorted_updates if u['codeVersion'] == latest_version]
+            latest_version_diffs = [u for u in latest_version_updates if u['isDiff'] == True]
+
+            if len(latest_version_diffs) > 0:
+                # Split out the deltas to the newest version from others and remove them
+                updates_to_delete, oldest_diff = self._split_newer_updates(latest_version_diffs)
+                for update_to_delete in updates_to_delete:
+                    print("Deleting delta %s version" % update_to_delete['fromVersion'])
+                    sorted_updates.remove(update_to_delete)
+
+                updates_to_delete = []
+                for update in sorted_updates:
+                    # Remove all full bundles that are not the version from the largest version delta
+                    if update['codeVersion'] != oldest_diff['fromVersion'] and update['isDiff'] == False:
+                        updates_to_delete.append(update)
+
+                    # Remove all deltas that don't target the latest version
+                    if update['codeVersion'] != oldest_diff['codeVersion'] and update['isDiff'] == True:
+                        updates_to_delete.append(update)
+
+                    # Remove all deltas that aren't from the only full update
+                    # XXX: Order of evalutation on this one is important since
+                    #      only diffs have 'fromVersion' field
+                    if update['isDiff'] == True and update['fromVersion'] != oldest_diff['fromVersion']:
+                        updates_to_delete.append(update)
+
+                for deletable_update in updates_to_delete:
+                    # We may have duplicates so we need this check here
+                    if deletable_update in sorted_updates:
+                        sorted_updates.remove(deletable_update)
+            else:
+                # Since this is a script for testing updates, if we have none in a
+                # bucket, then we don't need that bucket at all
+                sorted_updates = []
 
             for update in sorted_updates:
                 if update['isDiff']:
@@ -99,7 +157,21 @@ class DeltaUpdatesTool(object):
                 else:
                     print(" ", update['codeVersion'])
 
-        return unfiltered_updates
+            # Sanity check
+            # assert len(sorted_updates) == 0 or len(sorted_updates) == 2, \
+            #        "Resulting bucket results should always have either 0 or 2 results " \
+            #        "(got: %s)" % len(sorted_updates)
+
+            for update in sorted_updates:
+                if update['isDiff']:
+                    print(" ", "%s -> %s" % (update['fromVersion'], update['codeVersion']))
+                else:
+                    print(" ", update['codeVersion'])
+
+            # Tack on the current filtered bucket to the output list
+            filtered_updates += sorted_updates
+
+        return filtered_updates
 
     def save_json(self, location, updates):
         with open(location, 'wt') as json_file:
