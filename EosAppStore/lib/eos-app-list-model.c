4b397901c28363fdac06ec665c1d58b4418a0ce9
Emmanuele Bassi <ebassi@gnome.org>
2015-07-01 15:45:22 +0100

    model: Update the EAM transaction API
    EAM changed the transaction DBus API to list all the paths to the
    various bundle artifacts.
    
    [endlessm/eos-shell#5238]

--- EosAppStore/lib/eos-app-list-model.c
+++ EosAppStore/lib/eos-app-list-model.c
@@ -1117,8 +1117,16 @@ get_bundle_artifacts (EosAppListModel *self,
 
   eos_app_log_info_message ("Completing transaction with eam");
 
+  GVariantBuilder opts;
+  g_variant_builder_init (&opts, G_VARIANT_TYPE ("(a{sv})"));
+  g_variant_builder_open (&opts, G_VARIANT_TYPE ("a{sv}"));
+  g_variant_builder_add (&opts, "{sv}", "BundlePath", g_variant_new_string (bundle_path));
+  g_variant_builder_add (&opts, "{sv}", "SignaturePath", g_variant_new_string (signature_path));
+  g_variant_builder_add (&opts, "{sv}", "ChecksumPath", g_variant_new_string (sha256_path));
+  g_variant_builder_close (&opts);
+
   eos_app_manager_transaction_call_complete_transaction_sync (transaction,
-                                                              eos_get_bundle_download_dir (),
+                                                              g_variant_builder_end (&opts),
                                                               &retval,
                                                               cancellable,
                                                               &error);
