b01e708ee84efc622b76d08565db72bbef08cfd8
Jasper St. Pierre <jstpierre@mecheye.net>
2015-07-24 00:14:57 -0700

    Add a eos-download-bundle utility
    Extract the code from eos-app-list-model and put it into a simple
    command-line utility to to test with.

---
+++ EosAppStore/lib/eos-download-bundle.c
@@ -0,0 +1,110 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/* vim: set ai ts=2 sw=2 : */
+
+#include "config.h"
+
+#include "eos-downloader.h"
+
+#include <stdlib.h>
+
+static char **opt_app_ids;
+static gboolean opt_use_delta;
+static char *opt_output_dir = ".";
+
+static const GOptionEntry opt_entries[] = {
+  { "use-delta", 'd', 0, G_OPTION_ARG_NONE, &opt_use_delta, "Whether to download deltas, if they exist" },
+  { "output-dir", 'o', 0, G_OPTION_ARG_FILENAME, &opt_output_dir, "Where to download bundles to", "PATH" },
+  { G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_STRING_ARRAY, &opt_app_ids, "Application id", "APPID" },
+  { NULL },
+};
+
+static SoupSession *soup_session;
+static GHashTable *apps;
+
+static void
+download_app (EosAppInfo *info)
+{
+  g_print (" downloading...\n");
+
+  g_autofree char *bundle_path = eos_app_info_download_bundle (info, soup_session, opt_output_dir, opt_use_delta, NULL, NULL, NULL, NULL, NULL);
+  g_autofree char *sig_path = eos_app_info_download_signature (info, soup_session, opt_output_dir, opt_use_delta, NULL, NULL);
+  g_autofree char *hash_path = eos_app_info_create_sha256sum (info, opt_output_dir, opt_use_delta, bundle_path, NULL, NULL);
+
+  g_print ("  bundle: %s\n"
+           "  sig   : %s\n"
+           "  hash  : %s\n",
+           bundle_path, sig_path, hash_path);
+}
+
+static EosAppInfo *
+get_app_info_for_app_id (const char *app_id)
+{
+  EosAppInfo *info;
+
+  info = g_hash_table_lookup (apps, app_id);
+  if (info)
+    return info;
+
+  g_autofree char *with_desktop = g_strconcat (app_id, ".desktop", NULL);
+  info = g_hash_table_lookup (apps, with_desktop);
+  if (info)
+    return info;
+
+  return NULL;
+}
+
+static void
+download_app_id (const char *app_id)
+{
+  g_print ("%s:", app_id);
+
+  EosAppInfo *info = get_app_info_for_app_id (app_id);
+  if (info)
+    download_app (info);
+  else
+    g_print ("\n"
+             "  not found\n");
+
+  g_print ("\n");
+}
+
+static gboolean
+inited (gpointer user_data)
+{
+  g_autoptr(GError) error = NULL;
+
+  if (!eos_load_available_apps (apps, soup_session, NULL, &error))
+    {
+      g_printerr ("Could not load available apps: %s\n", error->message);
+      exit (EXIT_FAILURE);
+    }
+
+  char **app_id;
+  for (app_id = opt_app_ids; *app_id != NULL; app_id++)
+    download_app_id (*app_id);
+
+  exit (EXIT_SUCCESS);
+  return G_SOURCE_REMOVE;
+}
+
+int main(int argc, char *argv[])
+{
+  g_autoptr(GOptionContext) context = g_option_context_new (NULL);
+  g_option_context_add_main_entries (context, opt_entries, GETTEXT_PACKAGE);
+
+  apps = g_hash_table_new_full (g_str_hash, g_str_equal,
+                                g_free,
+                                (GDestroyNotify) g_object_unref);
+  soup_session = soup_session_new ();
+
+  if (!g_option_context_parse (context, &argc, &argv, NULL))
+    return EXIT_FAILURE;
+
+  if (!opt_app_ids)
+    return EXIT_FAILURE;
+
+  g_autoptr(GMainLoop) loop = g_main_loop_new (NULL, FALSE);
+  g_idle_add (inited, NULL);
+  g_main_loop_run (loop);
+  return EXIT_SUCCESS;
+}
---
+++ EosAppStore/lib/eos-downloader.c
@@ -0,0 +1,348 @@
+/* -*- mode: C; c-file-style: "gnu"; indent-tabs-mode: nil; -*- */
+/* vim: set ai ts=2 sw=2 : */
+
+#include "config.h"
+
+#include "eos-downloader.h"
+
+#include <glib/gstdio.h>
+#include <glib/gi18n-lib.h>
+
+#include "eos-app-log.h"
+#include "eos-app-utils.h"
+#include "eos-net-utils-private.h"
+
+char *
+eos_app_info_create_sha256sum (EosAppInfo *info,
+                               const char *download_dir,
+                               gboolean use_delta,
+                               const char *bundle_path,
+                               GCancellable *cancellable,
+                               GError **error_out)
+{
+  GError *error = NULL;
+  const char *bundle_hash = NULL;
+  const char *app_id = eos_app_info_get_application_id (info);
+
+  if (use_delta)
+    bundle_hash = eos_app_info_get_delta_bundle_hash (info);
+  else
+    bundle_hash = eos_app_info_get_bundle_hash (info);
+
+  if (bundle_hash == NULL || *bundle_hash == '\0')
+    {
+      g_set_error (error_out, EOS_APP_LIST_MODEL_ERROR,
+                   EOS_APP_LIST_MODEL_ERROR_CHECKSUM_MISSING,
+                   _("No verification available for app '%s'"),
+                   app_id);
+      return NULL;
+    }
+
+  char *sha256_name = g_strconcat (app_id, ".sha256", NULL);
+  char *sha256_path = g_build_filename (download_dir, sha256_name, NULL);
+  g_free (sha256_name);
+
+  gchar *contents = g_strconcat (bundle_hash, "\t", bundle_path, "\n", NULL);
+  if (!g_file_set_contents (sha256_path, contents, -1, &error))
+    g_propagate_error (error_out, error);
+
+  g_free (contents);
+
+  return sha256_path;
+}
+
+char *
+eos_app_info_download_signature (EosAppInfo *info,
+                                 SoupSession *soup_session,
+                                 const char *download_dir,
+                                 gboolean use_delta,
+                                 GCancellable *cancellable,
+                                 GError **error_out)
+{
+  GError *error = NULL;
+  const char *signature_uri = NULL;
+  const char *app_id = eos_app_info_get_application_id (info);
+
+  if (use_delta)
+    signature_uri = eos_app_info_get_delta_signature_uri (info);
+  else
+    signature_uri = eos_app_info_get_signature_uri (info);
+
+  if (signature_uri == NULL || *signature_uri == '\0')
+    {
+      g_set_error (error_out, EOS_APP_LIST_MODEL_ERROR,
+                   EOS_APP_LIST_MODEL_ERROR_SIGNATURE_MISSING,
+                   _("No signature available for app '%s'"),
+                   app_id);
+      return NULL;
+    }
+
+  char *signature_name = g_strconcat (app_id, ".asc", NULL);
+  char *signature_path = g_build_filename (download_dir, signature_name, NULL);
+  g_free (signature_name);
+
+  if (!eos_net_utils_download_file_with_retry (soup_session,
+                                               signature_uri, signature_path,
+                                               NULL, NULL, NULL,
+                                               cancellable, &error))
+    {
+      g_propagate_error (error_out, error);
+    }
+
+  return signature_path;
+}
+
+char *
+eos_app_info_download_bundle (EosAppInfo *info,
+                              SoupSession *soup_session,
+                              const char *download_dir,
+                              gboolean use_delta,
+                              GCancellable *cancellable,
+                              GFileProgressCallback progress_callback,
+                              gpointer progress_user_data,
+                              GDestroyNotify progress_destroy,
+                              GError **error_out)
+{
+  GError *error = NULL;
+  const char *bundle_uri = NULL;
+  const char *app_id = eos_app_info_get_application_id (info);
+
+  if (use_delta)
+    bundle_uri = eos_app_info_get_delta_bundle_uri (info);
+  else
+    bundle_uri = eos_app_info_get_bundle_uri (info);
+
+  eos_app_log_info_message ("Downloading - app id: %s (%s), bundle URI: %s",
+                            app_id,
+                            eos_app_info_get_available_version (info),
+                            bundle_uri);
+
+  if (bundle_uri == NULL || *bundle_uri == '\0')
+    {
+      eos_app_log_error_message ("Bundle URI is bad. Canceling");
+
+      g_set_error (error_out, EOS_APP_LIST_MODEL_ERROR,
+                   EOS_APP_LIST_MODEL_ERROR_FAILED,
+                   _("Application bundle '%s' could not be downloaded"),
+                   app_id);
+      return NULL;
+    }
+
+  char *extension = use_delta ? ".xdelta": ".bundle";
+  char *bundle_name = g_strconcat (app_id, extension, NULL);
+
+  char *bundle_path = g_build_filename (download_dir, bundle_name, NULL);
+  g_free (bundle_name);
+
+  eos_app_log_info_message ("Bundle save path is %s", bundle_path);
+
+  if (!eos_net_utils_download_file_with_retry (soup_session, bundle_uri,
+                                               bundle_path,
+                                               progress_callback,
+                                               progress_destroy,
+                                               progress_user_data,
+                                               cancellable, &error))
+    {
+      eos_app_log_error_message ("Download of bundle failed");
+
+      g_propagate_error (error_out, error);
+    }
+
+  return bundle_path;
+}
+
+static gint64
+get_local_updates_monotonic_id (void)
+{
+  char *url = eos_get_updates_meta_record_uri ();
+  char *target = eos_get_updates_meta_record_file ();
+  char *data = NULL;
+
+  GError *error = NULL;
+
+  gint64 monotonic_id = -1;
+
+  if (!g_file_get_contents (target, &data, NULL, &error))
+    {
+      eos_app_log_error_message ("Unable to load updates meta record: %s: %s!",
+                                 target,
+                                 error->message);
+
+      goto out;
+    }
+
+  if (!eos_app_load_updates_meta_record (&monotonic_id, data, NULL, &error))
+    {
+      eos_app_log_error_message ("Unable to parse updates meta record: %s: %s! "
+                                 "Removing file from system",
+                                 target,
+                                 error->message);
+
+      /* If we have parsing issues with the file, we want it removed from the
+       * system regardless of the reasons */
+      g_unlink (target);
+
+      goto out;
+    }
+
+out:
+  g_free (url);
+  g_free (target);
+  g_free (data);
+
+  g_clear_error (&error);
+
+  return monotonic_id;
+}
+
+static gboolean
+check_is_app_list_current (SoupSession *soup_session,
+                           GCancellable *cancellable)
+{
+  char *url = eos_get_updates_meta_record_uri ();
+  char *target = eos_get_updates_meta_record_file ();
+  char *data = NULL;
+
+  gint64 old_monotonic_id = 0;
+  gint64 monotonic_id = 0;
+  gboolean updates_current = FALSE;
+
+  GError *error = NULL;
+
+  eos_app_log_info_message ("Checking if app list update is needed");
+
+  eos_app_log_info_message ("Downloading updates meta record from: %s", url);
+  if (!eos_net_utils_download_file (soup_session,
+                                    "application/json",
+                                    url,
+                                    target,
+                                    &data,
+                                    TRUE, /* Use cached copy if we have it */
+                                    cancellable,
+                                    &error))
+    {
+      eos_app_log_error_message ("Unable to get updates meta record!");
+      goto out;
+    }
+
+  if (!eos_app_load_updates_meta_record (&monotonic_id, data, cancellable,
+                                         &error))
+    {
+      eos_app_log_error_message ("Unable to parse updates meta record! "
+                                 "Removing cached file.");
+
+      /* If we have parsing issues with the file, we want it removed from the
+       * system regardless of the reasons */
+      g_unlink (target);
+
+      goto out;
+    }
+
+  old_monotonic_id = get_local_updates_monotonic_id ();
+  eos_app_log_info_message ("Comparing monotonic update ID."
+                            " Old: %" G_GINT64_FORMAT ","
+                            " New: %" G_GINT64_FORMAT ".",
+                            old_monotonic_id,
+                            monotonic_id);
+
+  /* If monotonic IDs don't match, we need to update our app list */
+  if (monotonic_id == old_monotonic_id)
+    updates_current = TRUE;
+
+out:
+  eos_app_log_info_message ("App list updates are %scurrent",
+                            updates_current ? "" : "not ");
+
+  /* Clean up */
+  g_free (url);
+  g_free (target);
+  g_free (data);
+
+  if (error)
+    {
+      eos_app_log_error_message ("Failed checkng if update is needed!: %s. "
+                                 "Ignoring and assuming that update is needed",
+                                 error->message);
+
+      /* We eat the errors since we assume that it just means that
+       * we'll re-download the updates */
+      g_clear_error (&error);
+    }
+
+  return updates_current;
+}
+
+gboolean
+eos_load_available_apps (GHashTable *apps,
+                         SoupSession *soup_session,
+                         GCancellable *cancellable,
+                         GError **error_out)
+{
+  eos_app_log_debug_message ("Reloading available apps");
+
+  char *target = eos_get_updates_file ();
+  char *data = NULL;
+  gboolean updates_current;
+
+  GError *error = NULL;
+
+  updates_current = check_is_app_list_current (soup_session, cancellable);
+
+  /* Try a manual load of the data */
+  if (updates_current)
+    {
+      eos_app_log_info_message ("Loading cached updates.json");
+      if (!g_file_get_contents (target, &data, NULL, &error))
+        {
+          eos_app_log_error_message ("Loading cached updates.json failed. "
+                                     "Need to re-download it");
+
+          /* We clear the error because we want to force a re-download */
+          g_clear_error (&error);
+        }
+    }
+
+  /* If update needed or we couldn't load cached file, get it again */
+  if (!data)
+    {
+      char *url = eos_get_all_updates_uri ();
+      gboolean updates_dl_success;
+
+      eos_app_log_info_message ("Downloading list of available apps from: %s", url);
+      updates_dl_success = eos_net_utils_download_file (soup_session,
+                                                        "application/json",
+                                                        url,
+                                                        target,
+                                                        &data,
+                                                        FALSE, /* Don't use a cache if we have it */
+                                                        cancellable,
+                                                        &error);
+
+      g_free (url);
+
+      if (!updates_dl_success)
+        {
+          eos_app_log_error_message ("Download of all updates failed!");
+
+          g_free (target);
+          g_propagate_error (error_out, error);
+
+          return FALSE;
+        }
+    }
+
+  if (!eos_app_load_available_apps (apps, data, cancellable, &error))
+    {
+      eos_app_log_error_message ("Parsing of all updates failed!");
+
+      g_free (data);
+      g_propagate_error (error_out, error);
+
+      return FALSE;
+    }
+
+  g_free (data);
+  g_free (target);
+
+  return TRUE;
+}
