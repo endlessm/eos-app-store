e24f259712bba00d719265e3f7a33bf62e6364a7
Jasper St. Pierre <jstpierre@mecheye.net>
2015-08-13 00:00:45 -0700

    Update to new config file format used by eos-app-manager
    eos-app-manager changed config file locations from eam-default.cfg to
    eos-app-manager.ini. Instead of cargo-culting a bunch of find-replaces
    to keep this in sync, just copy over and hack up the eam-config file
    directly, so it will be easier to sync up in the future.

---
+++ EosAppStore/lib/eam-config.h
@@ -0,0 +1,23 @@
+/* Copyright 2014 Endless Mobile, Inc. */
+
+#ifndef EAM_CONFIG_H
+#define EAM_CONFIG_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+const char *    eam_config_get_applications_dir         (void);
+const char *    eam_config_get_primary_storage          (void);
+const char *    eam_config_get_secondary_storage        (void);
+const char *    eam_config_get_server_url               (void);
+const char *    eam_config_get_api_version              (void);
+gboolean        eam_config_get_enable_delta_updates     (void);
+
+gboolean        eam_config_set_key                      (const char *key,
+                                                         const char *value);
+gboolean        eam_config_reset_key                    (const char *key);
+
+G_END_DECLS
+
+#endif /* EAM_CONFIG_H */
