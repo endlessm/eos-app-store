a5e07ecfebf68fc2721c46c67416cfab623260df
Cosimo Cecchi <cosimo@endlessm.com>
2015-09-04 12:27:44 -0700

    appInstalledBox: fix missing parentheses
    [endlessm/eos-shell#5016]

--- EosAppStore/appInstalledBox.js
+++ EosAppStore/appInstalledBox.js
@@ -192,7 +192,7 @@ const AppInstalledBox = new Lang.Class({
                                              secondary_text: _("Deleting this app will remove it " +
                                                                "from the device for all users. You " +
                                                                "will need to download it from the " +
-                                                               "internet in order to reinstall it." });
+                                                               "internet in order to reinstall it.")});
         let applyButton = dialog.add_button(_("Delete app"), Gtk.ResponseType.APPLY);
         applyButton.get_style_context().add_class('destructive-action');
         dialog.add_button(_("Cancel"), Gtk.ResponseType.CANCEL);
