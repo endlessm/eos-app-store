fd74847ab3ede1a8441dc9cca8aaa54c1cca9913
Emmanuele Bassi <ebassi@gnome.org>
2016-01-15 17:11:37 +0000

    lib: Add EosAppInfo.application_dir getter
    [endlessm/eos-shell#6136]

--- EosAppStore/lib/eos-app-info.h
+++ EosAppStore/lib/eos-app-info.h
@@ -71,6 +71,7 @@ const char *    eos_app_info_get_checksum               (const EosAppInfo *info,
                                                          gboolean          use_delta,
                                                          GError          **error);
 const char *    eos_app_info_get_info_filename          (const EosAppInfo *info);
+GFile *         eos_app_info_get_application_dir        (const EosAppInfo *info);
 
 #ifndef __GI_SCANNER__
 
